6c660e6e739d57095ae7bb7e8f125988e7d6cf51
macpie <pierre@helium.com>
2018-10-23 16:12:42 -0700

    Add receipt txn

---
+++ src/transactions/blockchain_txn_poc_receipts.erl
@@ -0,0 +1,132 @@
+%%%-------------------------------------------------------------------
+%% @doc
+%% == Blockchain Transaction Proof of Coverage Receipts ==
+%%%-------------------------------------------------------------------
+-module(blockchain_txn_poc_receipts).
+
+-export([
+    new/2
+    ,receipts/1
+    ,signature/1
+    ,challenger/1
+    ,sign/2
+    ,is_valid/1
+    ,is/1
+]).
+
+-ifdef(TEST).
+-include_lib("eunit/include/eunit.hrl").
+-endif.
+
+-record(txn_poc_receipts, {
+    receipts :: blockchain_poc_receipt:poc_receipts()
+    ,challenger :: libp2p_crypto:address()
+    ,signature :: binary()
+}).
+
+-type txn_poc_receipts() :: #txn_poc_receipts{}.
+
+-export_type([txn_poc_receipts/0]).
+
+%%--------------------------------------------------------------------
+%% @doc
+%% @end
+%%--------------------------------------------------------------------
+-spec new(blockchain_poc_receipt:poc_receipts(), libp2p_crypto:address()) -> txn_poc_receipts().
+new(Receipts, Challenger) ->
+    #txn_poc_receipts{
+        receipts=Receipts
+        ,challenger=Challenger
+        ,signature = <<>>
+    }.
+
+%%--------------------------------------------------------------------
+%% @doc
+%% @end
+%%--------------------------------------------------------------------
+-spec receipts(txn_poc_receipts()) -> blockchain_poc_receipt:poc_receipts().
+receipts(Txn) ->
+    Txn#txn_poc_receipts.receipts.
+
+%%--------------------------------------------------------------------
+%% @doc
+%% @end
+%%--------------------------------------------------------------------
+-spec challenger(txn_poc_receipts()) -> libp2p_crypto:address().
+challenger(Txn) ->
+    Txn#txn_poc_receipts.challenger.
+
+%%--------------------------------------------------------------------
+%% @doc
+%% @end
+%%--------------------------------------------------------------------
+-spec signature(txn_poc_receipts()) -> binary().
+signature(Txn) ->
+    Txn#txn_poc_receipts.signature.
+
+%%--------------------------------------------------------------------
+%% @doc
+%% @end
+%%--------------------------------------------------------------------
+-spec sign(txn_poc_receipts(), libp2p_crypto:sig_fun()) -> txn_poc_receipts().
+sign(Txn, SigFun) ->
+    BinTxn = erlang:term_to_binary(Txn#txn_poc_receipts{signature = <<>>}),
+    Txn#txn_poc_receipts{signature=SigFun(BinTxn)}.
+
+%%--------------------------------------------------------------------
+%% @doc
+%% @end
+%%--------------------------------------------------------------------
+-spec is_valid(txn_poc_receipts()) -> boolean().
+is_valid(Txn=#txn_poc_receipts{challenger=Challenger, signature=Signature}) ->
+    PubKey = libp2p_crypto:address_to_pubkey(Challenger),
+    BinTxn = erlang:term_to_binary(Txn#txn_poc_receipts{signature = <<>>}),
+    libp2p_crypto:verify(BinTxn, Signature, PubKey).
+
+%%--------------------------------------------------------------------
+%% @doc
+%% @end
+%%--------------------------------------------------------------------
+-spec is(blockchain_transactions:transaction()) -> boolean().
+is(Txn) ->
+    erlang:is_record(Txn, txn_poc_receipts).
+
+%% ------------------------------------------------------------------
+%% EUNIT Tests
+%% ------------------------------------------------------------------
+-ifdef(TEST).
+
+new_test() ->
+    Tx = #txn_poc_receipts{
+        receipts=[]
+        ,challenger = <<"challenger">>
+        ,signature = <<>>
+    },
+    ?assertEqual(Tx, new([], <<"challenger">>)).
+
+receipts_test() ->
+    Tx = new([], <<"challenger">>),
+    ?assertEqual([], receipts(Tx)).
+
+challenger_test() ->
+    Tx = new([], <<"challenger">>),
+    ?assertEqual(<<"challenger">>, challenger(Tx)).
+
+signature_test() ->
+    Tx = new([], <<"challenger">>),
+    ?assertEqual(<<>>, signature(Tx)).
+
+sign_test() ->
+    {PrivKey, PubKey} = libp2p_crypto:generate_keys(),
+    Challenger = libp2p_crypto:pubkey_to_address(PubKey),
+    SigFun = libp2p_crypto:mk_sig_fun(PrivKey),
+    Tx0 = new([], Challenger),
+    Tx1 = sign(Tx0, SigFun),
+    Sig = signature(Tx1),
+    ?assert(libp2p_crypto:verify(erlang:term_to_binary(Tx1#txn_poc_receipts{signature = <<>>}), Sig, PubKey)).
+
+is_test() ->
+    Tx = new([], <<"challenger">>),
+    ?assert(is(Tx)).
+
+-endif.
