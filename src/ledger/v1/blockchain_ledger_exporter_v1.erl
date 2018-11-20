0cf5e65721a27737f4ce9fd894d1adec9a675a16
Rahul Garg <vihu89@gmail.com>
2018-11-20 15:51:47 +0530

    Fix xref warnings, use v1 for exporter as well

--- src/ledger/v1/blockchain_ledger_exporter_v1.erl
+++ src/ledger/v1/blockchain_ledger_exporter_v1.erl
@@ -15,7 +15,7 @@
 %% @doc
 %% @end
 %%--------------------------------------------------------------------
--spec export(blockchain_ledger:ledger()) -> any().
+-spec export(blockchain_ledger_v1:ledger()) -> any().
 export(Ledger) ->
     [
      {accounts, export_accounts(Ledger)},
@@ -27,24 +27,24 @@ export(Ledger) ->
 %% @doc
 %% @end
 %%--------------------------------------------------------------------
--spec export_accounts(blockchain_ledger:ledger()) -> any().
+-spec export_accounts(blockchain_ledger_v1:ledger()) -> any().
 export_accounts(Ledger) ->
     lists:foldl(fun({Address, Entry}, Acc) ->
                         [ [{address, libp2p_crypto:address_to_b58(Address)},
-                           {balance, blockchain_ledger:balance(Entry)}] | Acc ]
-                end, [], maps:to_list(blockchain_ledger:entries(Ledger))).
+                           {balance, blockchain_ledger_v1:balance(Entry)}] | Acc ]
+                end, [], maps:to_list(blockchain_ledger_v1:entries(Ledger))).
 
 %%--------------------------------------------------------------------
 %% @doc
 %% @end
 %%--------------------------------------------------------------------
--spec export_gateways(blockchain_ledger:ledger()) -> any().
+-spec export_gateways(blockchain_ledger_v1:ledger()) -> any().
 export_gateways(Ledger) ->
     lists:foldl(fun({GatewayAddress, Gateway}, Acc) ->
                         [ [{gateway_address, libp2p_crypto:address_to_b58(GatewayAddress)},
-                           {owner_address, libp2p_crypto:address_to_b58(blockchain_ledger_gateway:owner_address(Gateway))},
-                           {location, blockchain_ledger_gateway:location(Gateway)},
-                           {last_poc_challenge, blockchain_ledger_gateway:last_poc_challenge(Gateway)},
-                           {nonce, blockchain_ledger_gateway:nonce(Gateway)},
-                           {score, blockchain_ledger_gateway:score(Gateway)}] | Acc]
-                end, [], maps:to_list(blockchain_ledger:active_gateways(Ledger))).
+                           {owner_address, libp2p_crypto:address_to_b58(blockchain_ledger_gateway_v1:owner_address(Gateway))},
+                           {location, blockchain_ledger_gateway_v1:location(Gateway)},
+                           {last_poc_challenge, blockchain_ledger_gateway_v1:last_poc_challenge(Gateway)},
+                           {nonce, blockchain_ledger_gateway_v1:nonce(Gateway)},
+                           {score, blockchain_ledger_gateway_v1:score(Gateway)}] | Acc]
+                end, [], maps:to_list(blockchain_ledger_v1:active_gateways(Ledger))).
