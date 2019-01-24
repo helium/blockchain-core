5efbaa131d7f418ab0f21df27b823c2f6fa5aaf1
macpie <pierre@helium.com>
2019-01-24 15:15:38 -0800

    Simplify scoring for now

--- src/transactions/v1/blockchain_txn_poc_receipts_v1.erl
+++ src/transactions/v1/blockchain_txn_poc_receipts_v1.erl
@@ -113,49 +113,41 @@ absorb(Txn, Ledger) ->
         false ->
             {error, invalid_transaction};
         true ->
-            % TODO:
-                % 1. Get POC request (check last few blocks)
-                % 2. Validate poc request hash from secret
-                % 3. Re-create entropy target and path from secret and block hash
-            % TODO: Update score and last_poc_challenge
             case blockchain_ledger_v1:find_gateway_info(?MODULE:challenger(Txn), Ledger) of
                 {ok, ChallengerInfo} ->
                     case blockchain_ledger_gateway_v1:last_poc_challenge(ChallengerInfo) of
                         undefined ->
-                            lager:error("Challenger: ~p, Error: last_poc_challenge_undefined", [ChallengerInfo]),
+                            lager:error("challenger: ~p, error: last_poc_challenge_undefined", [ChallengerInfo]),
                             {error, last_poc_challenge_undefined};
-                        Height ->
+                        _Height ->
                             case blockchain_ledger_gateway_v1:last_poc_hash(ChallengerInfo) of
                                 undefined ->
-                                    lager:error("Challenger: ~p, Error: last_poc_undefined_undefined", [ChallengerInfo]),
+                                    lager:error("challenger: ~p, error: last_poc_undefined_undefined", [ChallengerInfo]),
                                     {error, last_poc_hash_undefined};
                                 Hash ->
                                     Secret = ?MODULE:secret(Txn),
                                     SecretHash = crypto:hash(sha256, Secret),
-                                    case Hash == SecretHash of
+                                    case Hash =:= SecretHash of
                                         true ->
-                                            lager:info("Txn: ~p", [Txn]),
-                                            lager:info("Challenger: ~p, last_poc_challenge at height: ~p, last_poc_hash: ~p", [ChallengerInfo, Height, Hash]),
-                                            lager:info("Hash: ~p, Secret: ~p", [Hash, Secret]),
-                                            lager:info("POC Receipt secret matches Challenger POC Request Hash!"),
-                                            %% XXX: This needs to be fixed and we need to have ledger snapshots or
-                                            %% rocksdb column families so we can build the path at the time the request
-                                            %% was mined.
-                                            {Target, ActiveGateways} = blockchain_poc_path:target(Secret, Ledger),
-                                            lager:info("Target: ~p", [Target]),
-                                            {ok, Path} = blockchain_poc_path:build(Target, ActiveGateways),
-                                            lager:info("Path: ~p", [Path]),
-                                            ReceiptAddrs = lists:sort([blockchain_poc_receipt_v1:address(R) || R <- ?MODULE:receipts(Txn)]),
-                                            case ReceiptAddrs == lists:sort(Path) andalso
-                                                 lists:member(Target, ReceiptAddrs) of
-                                                true ->
-                                                    lager:info("Got receipts from all the members in the path!");
-                                                false ->
-                                                    lager:error("Missing receipts!, ReconstructedPath: ~p, ReceivedReceipts: ~p", [Path, ReceiptAddrs]),
-                                                    {error, missing_receipts}
-                                            end;
+                                            ok;
+                                            % TODO: Once chain rewing ready use blockchain:ledger_at(Height) to grap
+                                            % ledger at time X. For now do nothing.
+
+                                            % {Target, ActiveGateways} = blockchain_poc_path:target(Secret, Ledger),
+                                            % lager:info("target: ~p", [Target]),
+                                            % {ok, Path} = blockchain_poc_path:build(Target, ActiveGateways),
+                                            % lager:info("path: ~p", [Path]),
+                                            % ReceiptAddrs = lists:sort([blockchain_poc_receipt_v1:address(R) || R <- ?MODULE:receipts(Txn)]),
+                                            % case ReceiptAddrs == lists:sort(Path) andalso
+                                            %      lists:member(Target, ReceiptAddrs) of
+                                            %     true ->
+                                            %         lager:info("got receipts from all the members in the path!");
+                                            %     false ->
+                                            %         lager:warning("missing receipts!, reconstructedPath: ~p, receivedReceipts: ~p", [Path, ReceiptAddrs]),
+                                            %         ok
+                                            % end;
                                         false ->
-                                            lager:error("POC Receipt secret ~p does not match Challenger POC Request Hash: ~p", [SecretHash, Hash]),
+                                            lager:error("POC receipt secret ~p does not match Challenger POC Request Hash: ~p", [SecretHash, Hash]),
                                             {error, incorrect_secret_hash}
                                     end
                             end
