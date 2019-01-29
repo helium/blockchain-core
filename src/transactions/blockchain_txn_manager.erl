c275495ca78a37dbe28504628a293b47c622bf94
Rahul Garg <vihu89@gmail.com>
2019-01-28 16:25:11 -0800

    Build txn queue bottom up

--- src/transactions/blockchain_txn_manager.erl
+++ src/transactions/blockchain_txn_manager.erl
@@ -31,10 +31,10 @@
         ]).
 
 -record(state, {
-          txn_map = #{} :: txn_map()
+          txn_queue :: txn_queue()
          }).
 
--type txn_map() :: #{blockchain_transactions:transaction() => erlang:queue()}.
+-type txn_queue() :: [{blockchain_transactions:transaction(), erlang:queue()}].
 
 %% ------------------------------------------------------------------
 %% API Function Definitions
@@ -48,7 +48,7 @@ start_link(Args) ->
 submit(Txn, ConsensusAddrs, Callback) ->
     gen_server:cast(?MODULE, {submit, Txn, ConsensusAddrs, Callback}).
 
--spec get_state() -> txn_map().
+-spec get_state() -> txn_queue().
 get_state() ->
     gen_server:call(?MODULE, get_state, infinity).
 
@@ -59,52 +59,47 @@ init(_Args) ->
     %% ok = blockchain_event:add_handler(self()),
     {ok, #state{}}.
 
-handle_cast({submit, Transaction, ConsensusAddrs, Callback}, State=#state{txn_map=TxnMap}) ->
-    F = (length(ConsensusAddrs) - 1) div 3,
-    RandomAddrs = random_n(F+1, ConsensusAddrs),
-    lager:info("blockchain_txn_manager, F: ~p, RandomAddrs: ~p", [F, RandomAddrs]),
-    NewTxnMap = maps:put(Transaction, queue:from_list(RandomAddrs), TxnMap),
-    lager:info("blockchain_txn_manager, NewTxnMap: ~p", [NewTxnMap]),
-    self() ! {process, Transaction, Callback},
-    {noreply, State#state{txn_map=NewTxnMap}};
+handle_cast({submit, Txn, ConsensusAddrs, Callback}, State=#state{txn_queue=TxnQueue}) ->
+    lager:info("blockchain_txn_manager, got Txn: ~p", [Txn]),
+    self() ! {process, Txn, ConsensusAddrs, Callback},
+    {noreply, State#state{txn_queue=[ {Txn, queue:new()} | TxnQueue]}};
 handle_cast(_Msg, State) ->
     lager:warning("blockchain_txn_manager got unknown cast: ~p", [_Msg]),
     {noreply, State}.
 
 handle_call(get_state, _from, State) ->
-    {reply, State#state.txn_map, State};
+    {reply, State#state.txn_queue, State};
 handle_call(_, _, State) ->
     {reply, ok, State}.
 
-handle_info({process, Txn, Callback}, State=#state{txn_map=TxnMap}) ->
-    lager:info("blockchain_txn_manager, process txn: ~p, TxnMap: ~p", [Txn, TxnMap]),
+handle_info({process, Txn, ConsensusAddrs, Callback}, State=#state{txn_queue=TxnQueue}) ->
+    lager:info("blockchain_txn_manager, process txn: ~p, TxnQueue: ~p", [Txn, TxnQueue]),
 
-    NewState = case maps:is_key(Txn, TxnMap) of
+    NewState = case lists:keyfind(Txn, 1, TxnQueue) of
                    false ->
-                       Callback({error, "Txn not found in State"}),
+                       Callback({error, "Txn not found in state: ~p", [Txn]}),
                        State;
-                   true ->
-                       TxnQueue = maps:get(Txn, TxnMap),
-                       case queue:is_empty(TxnQueue) of
+                   {Txn0, Queue} ->
+                       F = (length(ConsensusAddrs) - 1) div 3,
+                       case queue:len(Queue) == F + 1 of
                            true ->
+                               lager:info("blockchain_txn_manager, success dialing F+1 members, txn: ~p", [Txn]),
                                Callback(ok),
-                               State#state{txn_map=maps:remove(Txn, TxnMap)};
+                               State#state{txn_queue=lists:keydelete(Txn, 1, TxnQueue)};
                            false ->
-                               {value, AddrToDial} = queue:peek(TxnQueue),
-                               case dial(AddrToDial, Txn) of
+                               SuccesfulDialAddrs = queue:to_list(Queue),
+                               AddrsToSearch = ConsensusAddrs -- SuccesfulDialAddrs,
+                               RandomAddr = lists:nth(rand:uniform(length(AddrsToSearch)), AddrsToSearch),
+                               case dial(RandomAddr, Txn0) of
                                    ok ->
-                                       %% succesfful dial, pop txn from queue
-                                       lager:info("blockchain_txn_manager, popping address: ~p for txn: ~p", [AddrToDial, Txn]),
-                                       %% XXX: Get rid of this send_after
-                                       erlang:send_after(100, self(), {process, Txn, Callback}),
-                                       State#state{txn_map=maps:put(Txn, queue:drop(TxnQueue), TxnMap)};
+                                       lager:info("blockchain_txn_manager, Succesful dial to: ~p, txn: ~p, enqueuing", [RandomAddr, Txn]),
+                                       NewTxnQueue = lists:keyreplace(Txn, 1, TxnQueue, {Txn, queue:in(RandomAddr, Queue)}),
+                                       erlang:send_after(100, self(), {process, Txn, ConsensusAddrs, Callback}),
+                                       State#state{txn_queue=NewTxnQueue};
                                    Other ->
-                                       lager:error("Failed to dial txn: ~p, Error: ~p", [Txn, Other]),
-                                       %% TODO: drop this address and put a new one at the end of queue
-                                       %% XXX: Get rid of this send_after
-                                       erlang:send_after(100, self(), {process, Txn, Callback}),
-                                       %% Return same queue for now
-                                       State#state{txn_map=maps:put(Txn, TxnQueue, TxnMap)}
+                                       lager:error("blockchain_txn_manager, Failed dial to: ~p, txn: ~p, ignoring, Error: ~p", [RandomAddr, Txn, Other]),
+                                       erlang:send_after(100, self(), {process, Txn, ConsensusAddrs, Callback}),
+                                       State
                                end
                        end
                end,
@@ -121,23 +116,17 @@ code_change(_OldVsn, State, _Extra) ->
 %% ------------------------------------------------------------------
 %% Helper functions
 %% ------------------------------------------------------------------
-random_n(N, List) ->
-    lists:sublist(shuffle(List), N).
-
-shuffle(List) ->
-    [X || {_, X} <- lists:sort([{rand:uniform(), N} || N <- List])].
-
 dial(Addr, Txn) ->
     DataToSend = erlang:term_to_binary({blockchain_transactions:type(Txn), Txn}),
     P2PAddress = libp2p_crypto:address_to_p2p(Addr),
     Swarm = blockchain_swarm:swarm(),
     case libp2p_swarm:dial_framed_stream(Swarm, P2PAddress, ?TX_PROTOCOL, blockchain_txn_handler, [self()]) of
         {ok, Stream} ->
-            lager:info("dialed peer ~p via ~p~n", [Addr, ?TX_PROTOCOL]),
+            lager:info("blockchain_txn_manager, dialed peer ~p via ~p~n", [Addr, ?TX_PROTOCOL]),
             libp2p_framed_stream:send(Stream, DataToSend),
             libp2p_framed_stream:close(Stream),
             ok;
         Other ->
-            lager:notice("Failed to dial ~p service on ~p : ~p", [?TX_PROTOCOL, Addr, Other]),
+            lager:notice("blockchain_txn_manager, Failed to dial ~p service on ~p : ~p", [?TX_PROTOCOL, Addr, Other]),
             Other
     end.
