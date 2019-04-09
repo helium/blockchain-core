8b0afd67bfa1982ec7885aeb546e79380360023a
macpie <pierre@helium.com>
2019-04-09 13:20:40 -0700

    Add new txns to blockchain_txn

--- src/transactions/blockchain_txn.erl
+++ src/transactions/blockchain_txn.erl
@@ -14,9 +14,11 @@
 -type txn() :: blockchain_txn_add_gateway_v1:txn_add_gateway()
              | blockchain_txn_assert_location_v1:txn_assert_location()
              | blockchain_txn_coinbase_v1:txn_coinbase()
+             | blockchain_txn_security_coinbase_v1:txn_security_coinbase()
              | blockchain_txn_consensus_group_v1:txn_consensus_group()
              | blockchain_txn_gen_gateway_v1:txn_genesis_gateway()
              | blockchain_txn_payment_v1:txn_payment()
+             | blockchain_txn_security_exchange_v1:txn_security_exchange()
              | blockchain_txn_oui_v1:txn_oui()
              | blockchain_txn_create_htlc_v1:txn_create_htlc()
              | blockchain_txn_redeem_htlc_v1:txn_redeem_htlc()
@@ -53,15 +55,17 @@
 -define(BLOCK_DELAY, 50).
 -define(ORDER, [
     {blockchain_txn_coinbase_v1, 1},
-    {blockchain_txn_gen_gateway_v1, 2},
-    {blockchain_txn_payment_v1, 3},
-    {blockchain_txn_consensus_group_v1, 4},
-    {blockchain_txn_add_gateway_v1, 5},
-    {blockchain_txn_assert_location_v1, 6},
-    {blockchain_txn_create_htlc_v1, 7},
-    {blockchain_txn_redeem_htlc_v1, 8},
-    {blockchain_txn_poc_request_v1, 9},
-    {blockchain_txn_poc_receipts_v1, 10}
+    {blockchain_txn_security_coinbase_v1, 2},
+    {blockchain_txn_gen_gateway_v1, 3},
+    {blockchain_txn_payment_v1, 4},
+    {blockchain_txn_security_exchange_v1, 5},
+    {blockchain_txn_consensus_group_v1, 6},
+    {blockchain_txn_add_gateway_v1, 7},
+    {blockchain_txn_assert_location_v1, 8},
+    {blockchain_txn_create_htlc_v1, 9},
+    {blockchain_txn_redeem_htlc_v1, 10},
+    {blockchain_txn_poc_request_v1, 11},
+    {blockchain_txn_poc_receipts_v1, 12}
 ]).
 
 hash(Txn) ->
@@ -84,6 +88,8 @@ wrap_txn(#blockchain_txn_assert_location_v1_pb{}=Txn) ->
     #blockchain_txn_pb{txn={assert_location, Txn}};
 wrap_txn(#blockchain_txn_payment_v1_pb{}=Txn) ->
     #blockchain_txn_pb{txn={payment, Txn}};
+wrap_txn(#blockchain_txn_security_exchange_v1_pb{}=Txn) ->
+    #blockchain_txn_pb{txn={security_exchange, Txn}};
 wrap_txn(#blockchain_txn_create_htlc_v1_pb{}=Txn) ->
     #blockchain_txn_pb{txn={create_htlc, Txn}};
 wrap_txn(#blockchain_txn_redeem_htlc_v1_pb{}=Txn) ->
@@ -92,6 +98,8 @@ wrap_txn(#blockchain_txn_add_gateway_v1_pb{}=Txn) ->
     #blockchain_txn_pb{txn={add_gateway, Txn}};
 wrap_txn(#blockchain_txn_coinbase_v1_pb{}=Txn) ->
     #blockchain_txn_pb{txn={coinbase, Txn}};
+wrap_txn(#blockchain_txn_security_coinbase_v1_pb{}=Txn) ->
+    #blockchain_txn_pb{txn={security_coinbase, Txn}};
 wrap_txn(#blockchain_txn_consensus_group_v1_pb{}=Txn) ->
     #blockchain_txn_pb{txn={consensus_group, Txn}};
 wrap_txn(#blockchain_txn_poc_request_v1_pb{}=Txn) ->
@@ -217,6 +225,8 @@ type(#blockchain_txn_assert_location_v1_pb{}) ->
     blockchain_txn_assert_location_v1;
 type(#blockchain_txn_payment_v1_pb{}) ->
     blockchain_txn_payment_v1;
+type(#blockchain_txn_security_exchange_v1_pb{}) ->
+    blockchain_txn_security_exchange_v1;
 type(#blockchain_txn_create_htlc_v1_pb{}) ->
     blockchain_txn_create_htlc_v1;
 type(#blockchain_txn_redeem_htlc_v1_pb{}) ->
@@ -225,6 +235,8 @@ type(#blockchain_txn_add_gateway_v1_pb{}) ->
     blockchain_txn_add_gateway_v1;
 type(#blockchain_txn_coinbase_v1_pb{}) ->
     blockchain_txn_coinbase_v1;
+type(#blockchain_txn_security_coinbase_v1_pb{}) ->
+    blockchain_txn_security_coinbase_v1;
 type(#blockchain_txn_consensus_group_v1_pb{}) ->
     blockchain_txn_consensus_group_v1;
 type(#blockchain_txn_poc_request_v1_pb{}) ->
@@ -313,6 +325,8 @@ nonce(Txn) ->
             blockchain_txn_assert_location_v1:nonce(Txn);
         blockchain_txn_payment_v1 ->
             blockchain_txn_payment_v1:nonce(Txn);
+        blockchain_txn_security_exchange_v1 ->
+            blockchain_txn_security_exchange_v1:nonce(Txn);
         _ ->
             -1 %% other transactions sort first
     end.
@@ -328,6 +342,8 @@ actor(Txn) ->
             blockchain_txn_assert_location_v1:gateway(Txn);
         blockchain_txn_payment_v1 ->
             blockchain_txn_payment_v1:payer(Txn);
+        blockchain_txn_security_exchange_v1 ->
+            blockchain_txn_security_exchange_v1:payer(Txn);
         blockchain_txn_create_htlc_v1 ->
             blockchain_txn_create_htlc_v1:payer(Txn);
         blockchain_txn_redeem_htlc_v1 ->
@@ -338,6 +354,8 @@ actor(Txn) ->
             blockchain_txn_add_gateway_v1:owner(Txn);
         blockchain_txn_coinbase_v1 ->
             blockchain_txn_coinbase_v1:payee(Txn);
+        blockchain_txn_security_coinbase_v1 ->
+            blockchain_txn_security_coinbase_v1:payee(Txn);
         blockchain_txn_poc_receipts_v1 ->
             blockchain_txn_poc_receipts_v1:challenger(Txn);
         _ ->
