2557b0860b2bdd0bc263c16dc24c0d92c743f24d
Rahul Garg <3199183+vihu@users.noreply.github.com>
2019-04-12 11:31:27 -0700

    Txn manager rework WIP (#91)
    - Make txn_manager async
    - Update txn_handler response
    - Remove timing assumptions

--- src/cli/blockchain_cli_txn.erl
+++ src/cli/blockchain_cli_txn.erl
@@ -63,26 +63,28 @@ txn_queue_usage() ->
     ].
 
 txn_queue(["txn", "queue"], [], []) ->
-    case (catch blockchain_txn_manager:txn_queue()) of
+    case (catch blockchain_txn_manager:txn_map()) of
         {'EXIT', _} ->
             [clique_status:text("timeout")];
         [] ->
             [clique_status:text("empty")];
-        TxnQueue ->
-            R = format_txn_queue(TxnQueue),
+        TxnMap ->
+            R = format_txn_queue(TxnMap),
             [clique_status:table(R)]
     end;
 txn_queue([], [], []) ->
     usage.
 
-format_txn_queue(TxnQueue) ->
-    lists:map(fun({Txn, _Callback, AcceptQ, RejectQ}) ->
+format_txn_queue(TxnMap) ->
+    lists:map(fun({TxnHash, Entry}) ->
+                      Txn = blockchain_txn_manager:txn(Entry),
                       TxnMod = blockchain_txn:type(Txn),
-                      TxnHash = blockchain_txn:hash(Txn),
+                      Acceptors = blockchain_txn_manager:acceptors(Entry),
+                      Rejectors = blockchain_txn_manager:rejectors(Entry),
                       [
                        {txn_type, atom_to_list(TxnMod)},
                        {txn_hash, io_lib:format("~p", [libp2p_crypto:bin_to_b58(TxnHash)])},
-                       {accepted_by, integer_to_list(length(AcceptQ))},
-                       {rejected_by, integer_to_list(length(RejectQ))}
+                       {accepted_by, integer_to_list(length(Acceptors))},
+                       {rejected_by, integer_to_list(length(Rejectors))}
                       ]
-              end, TxnQueue).
+              end, maps:to_list(TxnMap)).
--- src/transactions/blockchain_txn_manager.erl
+++ src/transactions/blockchain_txn_manager.erl
@@ -14,8 +14,12 @@
 %% ------------------------------------------------------------------
 -export([
          start_link/1,
-         submit/3,
-         txn_queue/0
+         submit/2,
+         txn_map/0,
+         %% exports for the cli module
+         acceptors/1,
+         rejectors/1,
+         txn/1
         ]).
 
 %% ------------------------------------------------------------------
@@ -31,9 +35,8 @@
         ]).
 
 -record(state, {
-          txn_queue = [] :: txn_queue(),
-          chain,
-          counter = 0
+          txn_map = #{} :: txn_map(),
+          chain :: undefined | blockchain:blockchain()
          }).
 
 -record(entry, {
@@ -43,10 +46,10 @@
           callback :: fun()
          }).
 
--define(TIMEOUT, 5000).
+-define(LIMIT, 10).
 
 -type entry() :: #entry{}.
--type txn_queue() :: [entry()].
+-type txn_map() :: #{blockchain_txn:hash() => entry()}.
 -type pubkeys() :: [libp2p_crypto:pubkey_bin()].
 
 %% ------------------------------------------------------------------
@@ -55,13 +58,13 @@
 start_link(Args) ->
     gen_server:start_link({local, ?MODULE}, ?MODULE, Args, []).
 
--spec submit(Txn :: blockchain_txn:txn(), ConsensusAddrs :: pubkeys(), Callback :: fun()) -> ok.
-submit(Txn, ConsensusAddrs, Callback) ->
-    gen_server:cast(?MODULE, {submit, Txn, ConsensusAddrs, Callback}).
+-spec submit(Txn :: blockchain_txn:txn(), Callback :: fun()) -> ok.
+submit(Txn, Callback) ->
+    gen_server:cast(?MODULE, {submit, Txn, Callback}).
 
--spec txn_queue() -> txn_queue().
-txn_queue() ->
-    gen_server:call(?MODULE, txn_queue, 60000).
+-spec txn_map() -> txn_map().
+txn_map() ->
+    gen_server:call(?MODULE, txn_map, 60000).
 
 %% ------------------------------------------------------------------
 %% gen_server Function Definitions
@@ -71,126 +74,139 @@ init(_Args) ->
     Chain = blockchain_worker:blockchain(),
     {ok, #state{chain=Chain}}.
 
-handle_cast({submit, Txn, _ConsensusAddrs, Callback}, State=#state{txn_queue=TxnQueue, counter=Counter}) ->
-    SortedTxnQueue = lists:sort(fun(EntryA, EntryB) ->
-                                        blockchain_txn:sort(txn(EntryA), txn(EntryB))
-                                end, TxnQueue ++ [new_entry(Txn, Callback, [], [])]),
+handle_cast({submit, Txn, Callback}, State=#state{txn_map=TxnMap}) ->
+    TxnHash = blockchain_txn:hash(Txn),
+    NewTxnMap = maps:put(TxnHash, new_entry(Txn, Callback, [], []), TxnMap),
 
-    case (Counter + 1) rem 50 == 0 of
-        true ->
-            %% force the txn queue to flush every 50 messages
-            self() ! timeout;
-        false ->
-            ok
+    case maps:size(NewTxnMap) > ?LIMIT of
+        true -> self() ! timeout;
+        false -> ok
     end,
-    {noreply, State#state{txn_queue=SortedTxnQueue, counter=Counter+1}, ?TIMEOUT};
+
+    lager:info("Got Txn: ~p, TxnMap: ~p", [TxnHash, NewTxnMap]),
+
+    {noreply, State#state{txn_map=NewTxnMap}};
 handle_cast(_Msg, State) ->
     lager:warning("blockchain_txn_manager got unknown cast: ~p", [_Msg]),
-    {noreply, State, ?TIMEOUT}.
+    {noreply, State}.
 
-handle_call(txn_queue, _From, State=#state{txn_queue=TxnQueue}) ->
-    {reply, TxnQueue, State, ?TIMEOUT};
+handle_call(txn_map, _From, State=#state{txn_map=TxnMap}) ->
+    {reply, TxnMap, State};
 handle_call(_, _, State) ->
-    {reply, ok, State, ?TIMEOUT}.
+    {reply, ok, State}.
 
-%% handle_info(timeout, State=#state{txn_queue=[{_Txn, _Callback, _CallbackInfo, AcceptQueue0, _RejectQueue0} | _Tail]=TxnQueue, chain=Chain}) when Chain /= undefined ->
-handle_info(timeout, State=#state{txn_queue=[Head | _Tail]=TxnQueue, chain=Chain}) when Chain /= undefined ->
-    Ledger = blockchain:ledger(Chain),
-    {ok, ConsensusAddrs} = blockchain_ledger_v1:consensus_members(Ledger),
-    F = (length(ConsensusAddrs) - 1) div 3,
+handle_info(Msg, State = #state{chain=undefined}) ->
+    %% Keep trying to get a running chain
+    Chain = blockchain_worker:blockchain(),
+    self() ! Msg,
+    {noreply, State#state{chain=Chain}};
+handle_info(timeout, State=#state{txn_map=TxnMap, chain=Chain}) ->
+    {ok, ConsensusAddrs} = blockchain_ledger_v1:consensus_members(blockchain:ledger(Chain)),
     Swarm = blockchain_swarm:swarm(),
-    AddrsToSearch = ConsensusAddrs -- acceptors(Head),
-    RandomAddr = lists:nth(rand:uniform(length(AddrsToSearch)), AddrsToSearch),
-    P2PAddress = libp2p_crypto:pubkey_bin_to_p2p(RandomAddr),
-    Ref = make_ref(),
-    NewState = case libp2p_swarm:dial_framed_stream(Swarm, P2PAddress, ?TX_PROTOCOL, blockchain_txn_handler, [self(), Ref]) of
-                   {ok, Stream} ->
-                       NewTxnQueue = lists:foldl(fun(#entry{txn=Txn, acceptors=Acceptors, rejectors=Rejectors, callback=Callback}=Entry, Acc) ->
-                                                         case lists:member(RandomAddr, Acceptors) of
-                                                             false ->
-                                                                 DataToSend = blockchain_txn:serialize(Txn),
-                                                                 case libp2p_framed_stream:send(Stream, DataToSend) of
-                                                                     {error, Reason} ->
-                                                                         lager:error("blockchain_txn_manager, libp2p_framed_stream send failed: ~p, TxnQueueLen: ~p", [Reason, length(TxnQueue)]),
-                                                                         [Entry | Acc];
-                                                                     _ ->
-                                                                         receive
-                                                                             {Ref, ok} ->
-                                                                                 case length(Acceptors) > F of
-                                                                                     true ->
-                                                                                         invoke_callback(Callback, ok),
-                                                                                         Acc;
-                                                                                     false ->
-                                                                                         NewEntry = new_entry(Txn, Callback, [RandomAddr | Acceptors], Rejectors),
-                                                                                         [NewEntry | Acc]
-                                                                                 end;
-                                                                             {Ref, error} ->
-                                                                                 case length(Rejectors) > 2*F of
-                                                                                     true ->
-                                                                                         lager:error("blockchain_txn_manager, dropping txn: ~p rejected by 2F+1 members", [Txn]),
-                                                                                         invoke_callback(Callback, {error, rejected}),
-                                                                                         Acc;
-                                                                                     false ->
-                                                                                         lager:error("blockchain_txn_manager, txn: ~p reject by ~p, TxnQueueLen: ~p", [Txn, Stream, length(TxnQueue)]),
-                                                                                         NewEntry = new_entry(Txn, Callback, Acceptors, lists:usort([RandomAddr | Rejectors])),
-                                                                                         [NewEntry | Acc]
-                                                                                 end
-                                                                         after
-                                                                             30000 ->
-                                                                                 lager:warning("blockchain_txn_manager, txn: ~p TIMEOUT, TxnQueueLen: ~p", [Txn, length(TxnQueue)]),
-                                                                                 [Entry | Acc]
-                                                                         end
-                                                                 end;
-                                                             true ->
-                                                                 [Entry | Acc]
-                                                         end
-                                                 end, [], TxnQueue),
-                       libp2p_framed_stream:close(Stream),
-                       State#state{txn_queue=lists:reverse(NewTxnQueue)};
-                   _Other ->
-                       %% try to dial someone else ASAR
-                       erlang:send_after(?TIMEOUT, self(), timeout),
-                       State
-               end,
-    {noreply, NewState, ?TIMEOUT};
-handle_info({blockchain_event, {add_block, Hash, _Sync}}, State = #state{chain=Chain0, txn_queue=TxnQueue}) ->
-    Chain = case Chain0 of
-                undefined ->
-                    case blockchain_worker:blockchain() of
-                        undefined ->
-                            %% uncaught throws count as a return value in gen_servers
-                            throw({noreply, State});
-                        C ->
-                            C
-                    end;
-                C ->
-                    C
-            end,
 
+    SortedTxns = lists:sort(fun({_TxnHashA, EntryA}, {_TxnHashB, EntryB}) ->
+                                    blockchain_txn:sort(txn(EntryA), txn(EntryB))
+                            end, maps:to_list(TxnMap)),
+
+    ok = lists:foreach(fun({TxnHash, #entry{txn=Txn, acceptors=Acceptors}=_Entry}) ->
+                               AddrsToSearch = ConsensusAddrs -- Acceptors,
+                               RandomAddr = lists:nth(rand:uniform(length(AddrsToSearch)), AddrsToSearch),
+                               P2PAddress = libp2p_crypto:pubkey_bin_to_p2p(RandomAddr),
+
+                               case lists:member(RandomAddr, Acceptors) of
+                                   false ->
+                                       case libp2p_swarm:dial_framed_stream(Swarm,
+                                                                            P2PAddress,
+                                                                            ?TX_PROTOCOL,
+                                                                            blockchain_txn_handler,
+                                                                            [self(), TxnHash, RandomAddr]) of
+                                           {error, Reason} ->
+                                               lager:error("libp2p_framed_stream dial failed. Reason: ~p, To: ~p, TxnHash: ~p",
+                                                           [Reason, P2PAddress, TxnHash]);
+                                           {ok, Stream} ->
+                                               DataToSend = blockchain_txn:serialize(Txn),
+                                               case libp2p_framed_stream:send(Stream, DataToSend) of
+                                                   {error, Reason} ->
+                                                       lager:error("libp2p_framed_stream send failed. Reason: ~p, To: ~p, TxnHash: ~p",
+                                                                   [Reason, P2PAddress, TxnHash]);
+                                                   _ ->
+                                                       ok
+                                               end
+                                       end;
+                                   true ->
+                                       lager:info("ConsensusMember: ~p already has txn: ~p", [P2PAddress, TxnHash]),
+                                       ok
+                               end
+                       end,
+                       SortedTxns),
+
+    {noreply, State};
+handle_info({blockchain_txn_response, {ok, TxnHash, AcceptedBy}}, State=#state{txn_map=TxnMap, chain=Chain}) ->
+    {ok, ConsensusAddrs} = blockchain_ledger_v1:consensus_members(blockchain:ledger(Chain)),
+    F = (length(ConsensusAddrs) - 1) div 3,
+
+    NewTxnMap = case maps:get(TxnHash, TxnMap, undefined) of
+                    undefined ->
+                        TxnMap;
+                    #entry{acceptors=Acceptors, callback=Callback}=Entry ->
+                        case (length(Acceptors) + 1) > F of
+                            true ->
+                                lager:info("Reached acceptance threshold for txn: ~p, invoking ok callback", [TxnHash]),
+                                invoke_callback(Callback, ok),
+                                maps:remove(TxnHash, TxnMap);
+                            false ->
+                                maps:put(TxnHash, Entry#entry{acceptors=[AcceptedBy | Acceptors]}, TxnMap)
+                        end
+                end,
+
+    {noreply, State#state{txn_map=NewTxnMap}};
+handle_info({blockchain_txn_response, {error, TxnHash, RejectedBy}}, State=#state{chain=Chain, txn_map=TxnMap}) ->
+    {ok, ConsensusAddrs} = blockchain_ledger_v1:consensus_members(blockchain:ledger(Chain)),
+    F = (length(ConsensusAddrs) - 1) div 3,
+
+    NewTxnMap = case maps:get(TxnHash, TxnMap, undefined) of
+                    undefined ->
+                        TxnMap;
+                    #entry{rejectors=Rejectors, callback=Callback}=Entry ->
+                        case (length(Rejectors) + 1) > F of
+                            true ->
+                                lager:info("Reached rejection threshold for txn: ~p, invoking error callback", [TxnHash]),
+                                invoke_callback(Callback, {error, rejected}),
+                                maps:remove(TxnHash, TxnMap);
+                            false ->
+                                maps:put(TxnHash, Entry#entry{rejectors=[RejectedBy | Rejectors]}, TxnMap)
+                        end
+                end,
+    {noreply, State#state{txn_map=NewTxnMap}};
+handle_info({blockchain_event, {add_block, Hash, _Sync}}, State = #state{chain=Chain, txn_map=TxnMap}) ->
     case blockchain:get_block(Hash, Chain) of
         {ok, Block} ->
             Txns = blockchain_block:transactions(Block),
-            {_ValidTransactions, InvalidTransactions} = blockchain_txn:validate([txn(Entry) || Entry <- TxnQueue], Chain),
-            NewTxnQueue = lists:foldl(fun(#entry{txn=Txn, callback=Callback}=Entry, Acc) ->
-                                              case {lists:member(txn(Entry), Txns), lists:member(Txn, InvalidTransactions)} of
-                                                  {true, _} ->
+            {_ValidTransactions, InvalidTransactions} = blockchain_txn:validate([txn(Entry) || Entry <- maps:values(TxnMap)], Chain),
+            NewTxnMap = maps:filter(fun(_TxnHash, #entry{txn=Txn, callback=Callback}) ->
+                                            case {lists:member(Txn, Txns), lists:member(Txn, InvalidTransactions)} of
+                                                {true, _} ->
                                                       invoke_callback(Callback, ok),
-                                                      Acc;
-                                                  {_, true} ->
+                                                      false;
+                                                {_, true} ->
                                                       invoke_callback(Callback, {error, invalid}),
-                                                      Acc;
-                                                  _ ->
-                                                      [Entry | Acc]
-                                              end
-                                      end, [], TxnQueue),
+                                                      false;
+                                                _ ->
+                                                    true
+                                            end
+                                    end,
+                                    TxnMap),
 
-            {noreply, State#state{txn_queue=lists:reverse(NewTxnQueue), chain=Chain}, ?TIMEOUT};
+            self() ! timeout,
+
+            {noreply, State#state{txn_map=NewTxnMap}};
         _ ->
             %% this should not happen
-            error(missing_block)
+            lager:error("missing_block!"),
+            {noreply, State}
     end;
 handle_info(_Msg, State) ->
-    {noreply, State, ?TIMEOUT}.
+    {noreply, State}.
 
 terminate(_Reason, _State) ->
     ok.
@@ -207,6 +223,9 @@ txn(Entry) -> Entry#entry.txn.
 
 -spec acceptors(entry()) -> pubkeys().
 acceptors(Entry) -> Entry#entry.acceptors.
+
+-spec rejectors(entry()) -> pubkeys().
+rejectors(Entry) -> Entry#entry.rejectors.
 
 -spec new_entry(blockchain_txn:txn(), fun(), pubkeys(), pubkeys()) -> entry().
 new_entry(Txn, Callback, Acceptors, Rejectors) ->
