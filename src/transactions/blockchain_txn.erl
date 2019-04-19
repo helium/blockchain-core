60f3b64e361de3938727738bc8593af410fcd404
macpie <pierre@helium.com>
2019-04-19 12:44:00 -0700

    [ch2362] Add data credit burn txn

--- src/transactions/blockchain_txn.erl
+++ src/transactions/blockchain_txn.erl
@@ -24,7 +24,8 @@
              | blockchain_txn_create_htlc_v1:txn_create_htlc()
              | blockchain_txn_redeem_htlc_v1:txn_redeem_htlc()
              | blockchain_txn_poc_request_v1:txn_poc_request()
-             | blockchain_txn_poc_receipts_v1:txn_poc_receipts().
+             | blockchain_txn_poc_receipts_v1:txn_poc_receipts()
+             | blockchain_txn_data_credits_v1:txn_data_credits().
 -type txns() :: [txn()].
 -export_type([hash/0, txn/0, txns/0]).
 
@@ -277,7 +278,9 @@ type(#blockchain_txn_gen_gateway_v1_pb{}) ->
 type(#blockchain_txn_oui_v1_pb{}) ->
     blockchain_txn_oui_v1;
 type(#blockchain_txn_routing_v1_pb{}) ->
-    blockchain_txn_routing_v1.
+    blockchain_txn_routing_v1;
+type(#blockchain_txn_data_credits_v1_pb{}) ->
+    blockchain_txn_data_credits_v1.
 
 %% ------------------------------------------------------------------
 %% Internal Function Definitions
@@ -360,6 +363,8 @@ nonce(Txn) ->
             blockchain_txn_payment_v1:nonce(Txn);
         blockchain_txn_security_exchange_v1 ->
             blockchain_txn_security_exchange_v1:nonce(Txn);
+        blockchain_txn_data_credits_v1 ->
+            blockchain_txn_data_credits_v1:nonce(Txn);
         _ ->
             -1 %% other transactions sort first
     end.
@@ -395,6 +400,8 @@ actor(Txn) ->
             blockchain_txn_oui_v1:owner(Txn);
         blockchain_txn_routing_v1 ->
             blockchain_txn_routing_v1:owner(Txn);
+        blockchain_txn_data_credits_v1 ->
+            blockchain_txn_data_credits_v1:payer(Txn);
         _ ->
             <<>>
     end.
