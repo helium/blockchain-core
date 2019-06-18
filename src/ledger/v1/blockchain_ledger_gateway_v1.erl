5216763488e2c4419821a1894cdd2b39a88ee628
Rahul Garg <vihu89@gmail.com>
2019-06-18 16:17:31 -0700

    Add score decay eunits

--- src/ledger/v1/blockchain_ledger_gateway_v1.erl
+++ src/ledger/v1/blockchain_ledger_gateway_v1.erl
@@ -303,6 +303,19 @@ score_test() ->
     Gw = new(<<"owner_address">>, 12),
     ?assertEqual({1.0, 1.0, 0.25000000000000006}, score(Gw, 12)).
 
+score_decay_test() ->
+    Gw0 = new(<<"owner_address">>, 1),
+    Gw1 = set_alpha_beta_delta(1.1, 1.0, 300, Gw0),
+    ?assertEqual({1.0, 1.0, 0.25000000000000006}, score(Gw1, 1000)).
+
+score_decay2_test() ->
+    Gw0 = new(<<"owner_address">>, 1),
+    Gw1 = set_alpha_beta_delta(1.1, 10.0, 300, Gw0),
+    {Alpha, Beta, Score} = score(Gw1, 1000),
+    ?assertEqual(1.0, Alpha),
+    ?assert(Beta < 10.0),
+    ?assert(Score < 0.25).
+
 last_poc_challenge_test() ->
     Gw = new(<<"owner_address">>, 12),
     ?assertEqual(undefined, last_poc_challenge(Gw)),
