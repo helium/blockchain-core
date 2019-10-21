f5ee854c2bfbf4b204a875f5f4f8533acc3da7e3
Andrew Thompson <andrew@hijacked.us>
2019-10-20 18:54:24 -0700

    Honor rejections of transactions from F+1 members of consensus group
    Prior to this change the rejected transactions would be retried
    indefinitely.

--- src/transactions/blockchain_txn_mgr.erl
+++ src/transactions/blockchain_txn_mgr.erl
@@ -36,7 +36,7 @@
           chain :: undefined | blockchain:blockchain()
          }).
 
--type txn_map() :: #{blockchain_txn:txn() => {fun(), undefined | pid()}}.
+-type txn_map() :: #{blockchain_txn:txn() => {fun(), [], undefined | pid()}}.
 
 %% ------------------------------------------------------------------
 %% API Function Definitions
@@ -69,15 +69,15 @@ handle_cast({set_chain, Chain}, State=#state{chain=undefined}) ->
 handle_cast({submit, Txn, Callback}, State=#state{chain=undefined, txn_map=TxnMap}) ->
     %% Got txn when there is no chain
     %% Keep it in the txn_map and process when there is a chain
-    NewTxnMap = maps:put(Txn, {Callback, undefined}, TxnMap),
+    NewTxnMap = maps:put(Txn, {Callback, [], undefined}, TxnMap),
     self() ! wait_for_chain,
     {noreply, State#state{txn_map=NewTxnMap}};
 handle_cast({submit, Txn, Callback}, State=#state{chain=Chain, txn_map=TxnMap}) ->
     %% Get a random consensus member from the chain who signed the previous block
-    {ok, RandMember} = signatory_rand_member(Chain),
+    {ok, RandMember} = signatory_rand_member([], Chain),
     {ok, Dialer} = blockchain_txn_mgr_sup:start_dialer([self(), Txn, RandMember]),
     ok = blockchain_txn_dialer:dial(Dialer),
-    NewTxnMap = maps:put(Txn, {Callback, Dialer}, TxnMap),
+    NewTxnMap = maps:put(Txn, {Callback, [], Dialer}, TxnMap),
     {noreply, State#state{txn_map=NewTxnMap}};
 handle_cast(_Msg, State) ->
     lager:warning("blockchain_txn_mgr got unknown cast: ~p", [_Msg]),
@@ -106,9 +106,9 @@ handle_info(resubmit, State=#state{txn_map=TxnMap, chain=Chain}) ->
                                     blockchain_txn:sort(TxnA, TxnB)
                             end, maps:to_list(TxnMap)),
 
-    NewTxnMap = lists:foldl(fun({Txn, {Callback, Dialer}}, Acc) ->
+    NewTxnMap = lists:foldl(fun({Txn, {Callback, Rejections, Dialer}}, Acc) ->
                                     ok = blockchain_txn_mgr_sup:stop_dialer(Dialer),
-                                    {ok, RandMember} = signatory_rand_member(Chain),
+                                    {ok, RandMember} = signatory_rand_member(Rejections, Chain),
                                     {ok, NewDialer} = blockchain_txn_mgr_sup:start_dialer([self(), Txn, RandMember]),
                                     ok = blockchain_txn_dialer:dial(NewDialer),
                                     maps:put(Txn, {Callback, NewDialer}, Acc)
@@ -130,7 +130,7 @@ handle_info({send_failed, {Dialer, Txn, Member}}, State) ->
     {noreply, NewState};
 handle_info({rejected, {Dialer, Txn, Member}}, State) ->
     lager:debug("txn: ~p, rejected_by: ~p, Dialer: ~p", [Txn, Member, Dialer]),
-    NewState = retry(Txn, State),
+    NewState = rejected(Txn, Member, State),
     {noreply, NewState};
 handle_info({blockchain_event, {new_chain, NC}}, State) ->
     {noreply, State#state{chain = NC}};
@@ -146,7 +146,7 @@ handle_info({blockchain_event, {add_block, BlockHash, _Sync, _Ledger}}, State=#s
                                             blockchain_txn:sort(TxnA, TxnB)
                                     end, maps:to_list(TxnMap)),
 
-            NewTxnMap = lists:foldl(fun({Txn, {Callback, Dialer}}, Acc) ->
+            NewTxnMap = lists:foldl(fun({Txn, {Callback, Rejections, Dialer}}, Acc) ->
                                             case {lists:member(Txn, Txns), lists:member(Txn, InvalidTransactions)} of
                                                 {true, _} ->
                                                     ok = blockchain_txn_mgr_sup:stop_dialer(Dialer),
@@ -160,11 +160,11 @@ handle_info({blockchain_event, {add_block, BlockHash, _Sync, _Ledger}}, State=#s
                                                     %% Stop this dialer
                                                     lager:info("Rescheduling txn: ~p, stopping Dialer: ~p", [blockchain_txn:hash(Txn), Dialer]),
                                                     ok = blockchain_txn_mgr_sup:stop_dialer(Dialer),
-                                                    {ok, RandMember} = signatory_rand_member(Chain),
+                                                    {ok, RandMember} = signatory_rand_member(Rejections, Chain),
                                                     %% Retry with a new dialer
                                                     {ok, NewDialer} = blockchain_txn_mgr_sup:start_dialer([self(), Txn, RandMember]),
                                                     ok = blockchain_txn_dialer:dial(NewDialer),
-                                                    maps:put(Txn, {Callback, NewDialer}, Acc)
+                                                    maps:put(Txn, {Callback, Rejections, NewDialer}, Acc)
                                             end
                                     end,
                                     TxnMap,
@@ -190,22 +190,22 @@ code_change(_OldVsn, State, _Extra) ->
 invoke_callback(Callback, Msg) ->
     spawn(fun() -> Callback(Msg) end).
 
-signatory_rand_member(Chain) ->
+signatory_rand_member(Rejections, Chain) ->
     {ok, PrevBlock} = blockchain:head_block(Chain),
-    Signatures = blockchain_block:signatures(PrevBlock) -- [blockchain_swarm:pubkey_bin()],
-    case Signatures of
+    Signatories = [Signer || {Signer, _} <- blockchain_block:signatures(PrevBlock), not lists:member(Signer, [blockchain_swarm:pubkey_bin()|Rejections])],
+    case Signatories of
         [] ->
             %% rescue block! no signatures we can use
             %% so use a random consensus member
             Ledger = blockchain:ledger(Chain),
             {ok, Members0} = blockchain_ledger_v1:consensus_members(Ledger),
-            Members = Members0 -- [blockchain_swarm:pubkey_bin()],
+            Members = Members0 -- [blockchain_swarm:pubkey_bin()] -- Rejections,
             Index = rand:uniform(length(Members)),
             Member = lists:nth(Index, Members),
             {ok, Member};
         _ ->
-            Index = rand:uniform(length(Signatures)),
-            {Signer, _} = lists:nth(Index, Signatures),
+            Index = rand:uniform(length(Signatories)),
+            Signer= lists:nth(Index, Signatories),
             {ok, Signer}
     end.
 
@@ -214,11 +214,30 @@ retry(Txn, State=#state{txn_map=TxnMap, chain=Chain}) ->
         undefined ->
             %% We no longer have this txn, do nothing
             State;
-        {Callback, _Dialer} ->
+        {Callback, Rejections, _Dialer} ->
             %% Try a new one
-            {ok, NewRandMember} = signatory_rand_member(Chain),
+            {ok, NewRandMember} = signatory_rand_member(Rejections, Chain),
             {ok, NewDialer} = blockchain_txn_mgr_sup:start_dialer([self(), Txn, NewRandMember]),
             ok = blockchain_txn_dialer:dial(NewDialer),
-            NewTxnMap = maps:put(Txn, {Callback, NewDialer}, TxnMap),
+            NewTxnMap = maps:put(Txn, {Callback, Rejections, NewDialer}, TxnMap),
             State#state{txn_map=NewTxnMap}
+    end.
+
+rejected(Txn, Member, State=#state{txn_map=TxnMap, chain=Chain}) ->
+    case maps:get(Txn, TxnMap, undefined) of
+        undefined ->
+            %% We no longer have this txn, do nothing
+            State;
+        {Callback, Rejections, Dialer} ->
+            Ledger = blockchain:ledger(Chain),
+            {ok, Members} = blockchain_ledger_v1:consensus_members(Ledger),
+            N = length(Members),
+            F = (N - 1) div 3,
+            case length(lists:usort([Member|Rejections])) > F + 1 of
+                true ->
+                    %% too many rejections
+                    State#state{txn_map=maps:remove(Txn, TxnMap)};
+                false ->
+                    retry(Txn, State#state{txn_map=maps:put(Txn, {Callback, [Member|Rejections], Dialer}, TxnMap)})
+            end
     end.
