39fd71ddf8ecb93062764266d6e6846609713014
macpie <pierre@helium.com>
2019-11-04 11:33:03 -0800

    Update dcs_payment_req/dcs_payment

--- src/data_credits/blockchain_dcs_payment.erl
+++ src/data_credits/blockchain_dcs_payment.erl
@@ -6,60 +6,64 @@
 -module(blockchain_dcs_payment).
 
 -export([
-    payee/1, amount/1, nonce/1, signature/1,
-    new/3, sign/2, store/3, get_all/3,
-    encode/1,
-    decode/1
+    new/5,
+    payer/1, payee/1, amount/1, packets/1, nonce/1, signature/1,
+    sign/2, store/3, get_all/3,
+    encode/1, decode/1
 ]).
 
 -include("blockchain.hrl").
 -include("blockchain_dcs.hrl").
+-include_lib("helium_proto/src/pb/helium_dcs_payment_v1_pb.hrl").
 
 -ifdef(TEST).
 -include_lib("eunit/include/eunit.hrl").
 -endif.
 
--record(dcs_payment, {
-    payee :: binary(),
-    amount :: integer(),
-    nonce :: integer(),
-    signature :: binary() | undefined
-}).
+-type dcs_payment() ::#helium_dcs_payment_v1_pb{}.
 
--type dcs_payment() ::#dcs_payment{}.
-
--spec payee(dcs_payment()) -> binary().
-payee(#dcs_payment{payee=Payee}) ->
-    Payee.
-
--spec amount(dcs_payment()) -> integer().
-amount(#dcs_payment{amount=Amount}) ->
-    Amount.
-
--spec nonce(dcs_payment()) -> integer().
-nonce(#dcs_payment{nonce=Nonce}) ->
-    Nonce.
-
--spec signature(dcs_payment()) -> binary().
-signature(#dcs_payment{signature=Signature}) ->
-    Signature.
-
--spec new(binary(), non_neg_integer(), non_neg_integer()) -> dcs_payment().
-new(Payee, Amount, Nonce) -> 
-    #dcs_payment{
+-spec new(binary(), binary(), non_neg_integer(), binary(), non_neg_integer()) -> dcs_payment().
+new(Payer, Payee, Amount, Packets, Nonce) -> 
+    #helium_dcs_payment_v1_pb{
+        payer=Payer,
         payee=Payee,
         amount=Amount,
+        packets=Packets,
         nonce=Nonce
     }.
 
+-spec payer(dcs_payment()) -> binary().
+payer(#helium_dcs_payment_v1_pb{payer=Payer}) ->
+    Payer.
+
+-spec payee(dcs_payment()) -> binary().
+payee(#helium_dcs_payment_v1_pb{payee=Payee}) ->
+    Payee.
+
+-spec amount(dcs_payment()) -> non_neg_integer().
+amount(#helium_dcs_payment_v1_pb{amount=Amount}) ->
+    Amount.
+
+-spec packets(dcs_payment()) -> binary().
+packets(#helium_dcs_payment_v1_pb{packets=Packets}) ->
+    Packets.
+
+-spec nonce(dcs_payment()) -> non_neg_integer().
+nonce(#helium_dcs_payment_v1_pb{nonce=Nonce}) ->
+    Nonce.
+
+-spec signature(dcs_payment()) -> binary().
+signature(#helium_dcs_payment_v1_pb{signature=Signature}) ->
+    Signature.
+
 -spec sign(dcs_payment(), function()) -> dcs_payment().
 sign(Payment, SigFun) ->
-    EncodedPayment = ?MODULE:encode(Payment#dcs_payment{signature=undefined}),
+    EncodedPayment = ?MODULE:encode(Payment#helium_dcs_payment_v1_pb{signature= <<>>}),
     Signature = SigFun(EncodedPayment),
-    Payment#dcs_payment{signature=Signature}.
+    Payment#helium_dcs_payment_v1_pb{signature=Signature}.
 
 -spec store(rocksdb:db_handle(), rocksdb:cf_handle(), dcs_payment()) -> ok | {error, any()}.
-store(DB, CF, #dcs_payment{nonce=Nonce, amount=Amount}=Payment) ->
+store(DB, CF, #helium_dcs_payment_v1_pb{nonce=Nonce, amount=Amount}=Payment) ->
     Encoded = ?MODULE:encode(Payment),
     {ok, Batch} = rocksdb:batch(),
     ok = rocksdb:batch_put(Batch, CF, <<Nonce>>, Encoded),
@@ -97,12 +101,12 @@ get_all(DB, CF, Height, I, Payments) ->
     end.
 
 -spec encode(dcs_payment()) -> binary().
-encode(#dcs_payment{}=Payment) ->
-    erlang:term_to_binary(Payment).
+encode(#helium_dcs_payment_v1_pb{}=Payment) ->
+    helium_dcs_payment_v1_pb:encode_msg(Payment).
 
 -spec decode(binary()) -> dcs_payment().
 decode(BinaryPayment) ->
-    erlang:binary_to_term(BinaryPayment).
+    helium_dcs_payment_v1_pb:decode_msg(BinaryPayment, helium_dcs_payment_v1_pb).
 
 %% ------------------------------------------------------------------
 %% Internal Function Definitions
