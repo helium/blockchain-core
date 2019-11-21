a5ec3465a8f391c08fdeccb69339d63af9b60fdf
macpie <pierre@helium.com>
2019-11-20 16:35:01 -0800

    Add pending request check

--- src/state_channel/v1/blockchain_state_channel_message_v1.erl
+++ src/state_channel/v1/blockchain_state_channel_message_v1.erl
@@ -52,6 +52,12 @@ unwrap_msg(#helium_state_channel_message_v1_pb{msg={Type, Msg}}) ->
 
 encode_decode_test() ->
     Req = blockchain_state_channel_payment_req_v1:new(<<"id">>, <<"payee">>, 1, 12),
-    ?assertEqual({payment_req, Req}, decode(encode(Req))).
+    ?assertEqual({payment_req, Req}, decode(encode(Req))),
+
+    ReqID = base64:encode(crypto:strong_rand_bytes(32)),
+    Payment = blockchain_state_channel_payment_v1:new(<<"payer">>, <<"payee">>, 1, ReqID),
+    SC0 = blockchain_state_channel_v1:new(<<"1">>, <<"owner">>),
+    SC1 = blockchain_state_channel_v1:payments([{ReqID, Payment}], SC0),
+    ?assertEqual({state_channel, SC1}, decode(encode(SC1))).
 
 -endif.
