d5e601a211eb1bab19e94b9b9f2091ba2566c64c
Andrew Thompson <andrew@hijacked.us>
2019-11-27 18:46:42 -0800

    Attempt proper ledger snapshot for add block event

--- src/blockchain.erl
+++ src/blockchain.erl
@@ -542,16 +542,16 @@ add_block_(Block, Blockchain, Syncing) ->
                                                         lager:info("adding block ~p", [Height]),
                                                         ok = ?save_block(Block, Blockchain)
                                                     end,
-                                                    case blockchain_ledger_v1:new_snapshot(Ledger) of
+                                                    case blockchain_txn:absorb_and_commit(Block, Blockchain, BeforeCommit, Rescue) of
                                                         {error, Reason}=Error ->
-                                                            lager:error("Error creating snapshot, Reason: ~p", [Reason]),
+                                                            lager:error("Error absorbing transaction, Ignoring Hash: ~p, Reason: ~p", [blockchain_block:hash_block(Block), Reason]),
                                                             Error;
-                                                        {ok, NewLedger} ->
-                                                            case blockchain_txn:absorb_and_commit(Block, Blockchain, BeforeCommit, Rescue) of
+                                                        ok ->
+                                                            case blockchain_ledger_v1:new_snapshot(blockchain:ledger(Blockchain)) of
                                                                 {error, Reason}=Error ->
-                                                                    lager:error("Error absorbing transaction, Ignoring Hash: ~p, Reason: ~p", [blockchain_block:hash_block(Block), Reason]),
+                                                                    lager:error("Error creating snapshot, Reason: ~p", [Reason]),
                                                                     Error;
-                                                                ok ->
+                                                                {ok, NewLedger} ->
                                                                     lager:info("Notifying new block ~p", [Height]),
                                                                     ok = blockchain_worker:notify({add_block, Hash, Syncing, NewLedger})
                                                             end
