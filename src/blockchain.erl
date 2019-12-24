cfd3dd7743546bb350dec31a60ef945b9857d45a
Andrew Thompson <andrew@hijacked.us>
2019-12-23 16:04:19 -0800

    Resync the chain if the ledger is missing or at height 0

--- src/blockchain.erl
+++ src/blockchain.erl
@@ -1527,6 +1527,11 @@ maybe_continue_resync(Blockchain, Blocking) ->
 resync_fun(ChainHeight, LedgerHeight, Blockchain) ->
     blockchain_lock:acquire(),
     case get_block(LedgerHeight, Blockchain) of
+        {error, _} when LedgerHeight == 0 ->
+            %% reload the genesis block
+            {ok, GenesisBlock} = blockchain:genesis_block(Blockchain),
+            ok = blockchain_txn:absorb_and_commit(GenesisBlock, Blockchain, fun() -> ok end),
+            resync_fun(Blockchain, LedgerHeight + 1, ChainHeight);
         {error, _} ->
             %% chain is missing the block the ledger is stuck at
             %% it is unclear what we should do here.
@@ -1565,7 +1570,6 @@ resync_fun(ChainHeight, LedgerHeight, Blockchain) ->
                     end
             end
     end.
-
 
 %% ------------------------------------------------------------------
 %% EUNIT Tests
