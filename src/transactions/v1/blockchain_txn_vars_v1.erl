90d1796b1a5133af9854d284f26ed02d224c32ee
Evan Vigil-McClanahan <evan@helium.com>
2020-01-21 16:40:33 -0800

    WIP: Add var hooks

--- src/transactions/v1/blockchain_txn_vars_v1.erl
+++ src/transactions/v1/blockchain_txn_vars_v1.erl
@@ -590,6 +590,7 @@ delayed_absorb(Txn, Ledger) ->
     Vars = decode_vars(vars(Txn)),
     Unsets = decode_unsets(unsets(Txn)),
     ok = blockchain_ledger_v1:vars(Vars, Unsets, Ledger),
+    ok = process_hooks(Vars, Unsets, Ledger),
     case blockchain:config(?use_multi_keys, Ledger) of
         {ok, true} ->
             case multi_keys(Txn) of
@@ -1319,6 +1320,43 @@ invalid_var(Var, Value) ->
 invalid_var(Var, Value) ->
     throw({error, {unknown_var, Var, Value}}).
 -endif.
+
+
+process_hooks(Vars, Unsets, Ledger) ->
+    _ = maps:map(
+          fun(Var, Value) ->
+                  var_hook(Var, Value, Ledger)
+          end, Vars),
+    _ = lists:foreach(
+          fun(Var) ->
+                  unset_hook(Var, Ledger)
+          end, Unsets),
+    ok.
+
+%% separate out hook functions and call them in separate functions
+%% below the hook section.
+var_hook(?sweep_neighbors, true, Ledger) ->
+    neighbor_sweep(Ledger),
+    ok;
+var_hook(_Var, _Value, _Ledger) ->
+    ok.
+
+unset_hook(_Var, _Ledger) ->
+    ok.
+
+
+neighbor_sweep(Ledger) ->
+    case blockchain_ledger_v1:config(?poc_version, Ledger) of
+        {ok, V} when V > 3 ->
+            Gateways = blockchain_ledger_v1:active_gateways(Ledger),
+            %% find all neighbors for everyone
+            maps:map(
+              fun(A, G) ->
+                      G1 = blockchain_ledger_gateway_v2:neighbors([], G),
+                      blockchain_ledger_v1:update_gateway(G1, A, Ledger)
+              end, Gateways);
+        _ -> ok
+    end.
 
 %% ------------------------------------------------------------------
 %% EUNIT Tests
