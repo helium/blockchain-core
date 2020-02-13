26a9b904b557ae1678aab9d2fdf20d1ca1375bdb
Rahul Garg <rg@vihu.dev>
2020-02-13 15:07:26 -0800

    More tweaks to path_element_v2 and gateway_v3

--- src/transactions/v2/blockchain_poc_path_element_v2.erl
+++ src/transactions/v2/blockchain_poc_path_element_v2.erl
@@ -9,8 +9,8 @@
 -export([
     new/3,
     challengee/1,
-    receipt/1,
-    witnesses/1,
+    receipt/1, add_receipt/2,
+    witnesses/1, add_witness/2,
     print/1
 ]).
 
@@ -23,10 +23,6 @@
 
 -export_type([poc_element/0, poc_path/0]).
 
-%%--------------------------------------------------------------------
-%% @doc
-%% @end
-%%--------------------------------------------------------------------
 -spec new(Challengee :: libp2p_crypto:pubkey_bin(),
           Receipt :: blockchain_poc_receipt_v2:poc_receipt() | undefined,
           Witnesses :: blockchain_poc_witness_v2:poc_witnesses()) -> poc_element().
@@ -37,26 +33,49 @@ new(Challengee, Receipt, Witnesses) ->
         witnesses=Witnesses
     }.
 
-%%--------------------------------------------------------------------
-%% @doc
-%% @end
-%%--------------------------------------------------------------------
 -spec challengee(Element :: poc_element()) -> libp2p_crypto:pubkey_bin().
 challengee(Element) ->
     Element#blockchain_poc_path_element_v2_pb.challengee.
 
-%%--------------------------------------------------------------------
-%% @doc
-%% @end
-%%--------------------------------------------------------------------
 -spec receipt(Element :: poc_element()) -> blockchain_poc_receipt_v2:poc_receipt() | undefined.
 receipt(Element) ->
     Element#blockchain_poc_path_element_v2_pb.receipt.
 
-%%--------------------------------------------------------------------
-%% @doc
-%% @end
-%%--------------------------------------------------------------------
+-spec add_receipt(Element :: poc_element(),
+                  Receipt :: blockchain_poc_receipt_v2:poc_receipt()) -> poc_element().
+add_receipt(Element, Receipt) ->
+    case receipt(Element) of
+        R when R == Receipt ->
+            %% Readding the same receipt should not be allowed
+            {error, receipt_replay};
+        _ ->
+            Element#blockchain_poc_path_element_v2_pb{receipt=Receipt}
+    end.
+
+-spec add_witness(Element :: poc_element(),
+                  Witness :: blockchain_poc_witness_v2:poc_witness()) -> poc_element().
+add_witness(Element = #blockchain_poc_path_element_v2_pb{witnesses=Witnesses}, Witness) ->
+    %% Only add a witness to this path element if:
+    %% 1. The number of witnesses for this path element is less than 5
+    %% 2. It has not been a witness for this path element earlier
+    case has_witness(Element, Witness) of
+        false ->
+            case length(witnesses(Element)) < 5 of
+                false ->
+                    {error, max_witnesses};
+                true ->
+                    %% ok to add
+                    Element#blockchain_poc_path_element_v2_pb{witnesses=lists:sort([Witness | Witnesses])}
+            end;
+        true ->
+            {error, witness_already_added}
+    end.
+
+-spec has_witness(Element :: poc_element(),
+                  Witness :: blockchain_poc_witness_v2:poc_witness()) -> boolean().
+has_witness(#blockchain_poc_path_element_v2_pb{witnesses=Witnesses}, Witness) ->
+    lists:member(Witness, Witnesses).
+
 -spec witnesses(Element :: poc_element()) -> blockchain_poc_witness_v2:poc_witnesses().
 witnesses(Element) ->
     Element#blockchain_poc_path_element_v2_pb.witnesses.
