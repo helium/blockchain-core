46e6c6bdc953a01bb05b6b90a565a90a101c1018
Rahul Garg <rg@vihu.dev>
2020-02-26 10:54:54 -0800

    WIP: Add nonce to state_channel_open_v1 txns

--- src/ledger/v1/blockchain_ledger_state_channel_v1.erl
+++ src/ledger/v1/blockchain_ledger_state_channel_v1.erl
@@ -6,10 +6,11 @@
 -module(blockchain_ledger_state_channel_v1).
 
 -export([
-    new/4,
+    new/5,
     id/1, id/2,
     owner/1, owner/2,
     amount/1, amount/2,
+    nonce/1, nonce/2,
     expire_at_block/1, expire_at_block/2,
     serialize/1, deserialize/1
 ]).
@@ -22,30 +23,37 @@
     id :: binary(),
     owner :: binary(),
     amount :: non_neg_integer(),
-    expire_at_block :: pos_integer()
+    expire_at_block :: pos_integer(),
+    nonce :: non_neg_integer()
 }).
 
 -type state_channel() :: #ledger_state_channel_v1{}.
 
 -export_type([state_channel/0]).
 
--spec new(binary(), binary(), non_neg_integer(), pos_integer()) -> state_channel().
-new(ID, Owner, Amount, Timer) when ID /= undefined andalso
-                                   Owner /= undefined andalso
-                                   Amount /= undefined andalso
-                                   Timer /= undefined ->
+-spec new(ID :: binary(),
+          Owner :: binary(),
+          Amount :: non_neg_integer(),
+          ExpireAtBlock :: pos_integer(),
+          Nonce :: non_neg_integer()) -> state_channel().
+new(ID, Owner, Amount, ExpireAtBlock, Nonce) when ID /= undefined andalso
+                                                  Owner /= undefined andalso
+                                                  Amount /= undefined andalso
+                                                  ExpireAtBlock /= undefined andalso
+                                                  Nonce /= undefined ->
     #ledger_state_channel_v1{
-        id=ID,
-        owner=Owner,
-        amount=Amount,
-        expire_at_block=Timer
-    }.
+       id=ID,
+       owner=Owner,
+       amount=Amount,
+       expire_at_block=ExpireAtBlock,
+       nonce=Nonce
+      }.
 
 -spec id(state_channel()) -> binary().
 id(#ledger_state_channel_v1{id=ID}) ->
     ID.
 
--spec id(binary(), state_channel()) -> state_channel().
+-spec id(ID :: binary(), SC :: state_channel()) -> state_channel().
 id(ID, SC) ->
     SC#ledger_state_channel_v1{id=ID}.
 
@@ -53,7 +61,7 @@ id(ID, SC) ->
 owner(#ledger_state_channel_v1{owner=Owner}) ->
     Owner.
 
--spec owner(binary(), state_channel()) -> state_channel().
+-spec owner(Owner :: binary(), SC :: state_channel()) -> state_channel().
 owner(Owner, SC) ->
     SC#ledger_state_channel_v1{owner=Owner}.
 
@@ -61,17 +69,25 @@ owner(Owner, SC) ->
 amount(#ledger_state_channel_v1{amount=Amount}) ->
     Amount.
 
--spec amount(non_neg_integer(), state_channel()) -> state_channel().
+-spec amount(Amount :: non_neg_integer(), SC :: state_channel()) -> state_channel().
 amount(Amount, SC) ->
     SC#ledger_state_channel_v1{amount=Amount}.
 
 -spec expire_at_block(state_channel()) -> pos_integer().
-expire_at_block(#ledger_state_channel_v1{expire_at_block=Timer}) ->
-    Timer.
+expire_at_block(#ledger_state_channel_v1{expire_at_block=ExpireAtBlock}) ->
+    ExpireAtBlock.
 
--spec expire_at_block(pos_integer(), state_channel()) -> state_channel().
-expire_at_block(Timer, SC) ->
-    SC#ledger_state_channel_v1{expire_at_block=Timer}.
+-spec expire_at_block(ExpireAtBlock :: pos_integer(), SC :: state_channel()) -> state_channel().
+expire_at_block(ExpireAtBlock, SC) ->
+    SC#ledger_state_channel_v1{expire_at_block=ExpireAtBlock}.
+
+-spec nonce(state_channel()) -> non_neg_integer().
+nonce(#ledger_state_channel_v1{nonce=Nonce}) ->
+    Nonce.
+
+-spec nonce(Nonce :: non_neg_integer(), SC :: state_channel()) -> state_channel().
+nonce(Nonce, SC) ->
+    SC#ledger_state_channel_v1{nonce=Nonce}.
 
 %%--------------------------------------------------------------------
 %% @doc
@@ -106,28 +122,34 @@ new_test() ->
         id = <<"id">>,
         owner = <<"owner">>,
         amount = 1,
-        expire_at_block = 10
+        expire_at_block = 10,
+        nonce = 1
     },
-    ?assertEqual(SC, new(<<"id">>, <<"owner">>, 1, 10)).
+    ?assertEqual(SC, new(<<"id">>, <<"owner">>, 1, 10, 1)).
 
 id_test() ->
-    SC = new(<<"id">>, <<"owner">>, 1, 10),
+    SC = new(<<"id">>, <<"owner">>, 1, 10, 1),
     ?assertEqual(<<"id">>, id(SC)),
     ?assertEqual(<<"id2">>, id(id(<<"id2">>, SC))).
 
 owner_test() ->
-    SC = new(<<"id">>, <<"owner">>, 1, 10),
+    SC = new(<<"id">>, <<"owner">>, 1, 10, 1),
     ?assertEqual(<<"owner">>, owner(SC)),
     ?assertEqual(<<"owner2">>, owner(owner(<<"owner2">>, SC))).
 
 amount_test() ->
-    SC = new(<<"id">>, <<"owner">>, 1, 10),
+    SC = new(<<"id">>, <<"owner">>, 1, 10, 1),
     ?assertEqual(1, amount(SC)),
     ?assertEqual(2, amount(amount(2, SC))).
 
 expire_at_block_test() ->
-    SC = new(<<"id">>, <<"owner">>, 1, 10),
+    SC = new(<<"id">>, <<"owner">>, 1, 10, 1),
     ?assertEqual(10, expire_at_block(SC)),
     ?assertEqual(20, expire_at_block(expire_at_block(20, SC))).
+
+nonce_test() ->
+    SC = new(<<"id">>, <<"owner">>, 1, 10, 1),
+    ?assertEqual(1, nonce(SC)),
+    ?assertEqual(2, nonce(nonce(2, SC))).
 
 -endif.
