613d20cdd9bc1bb6ee251ea65a392b4c2efb248f
Andrew Thompson <andrew@hijacked.us>
2020-04-06 19:51:25 -0700

    Add some txn field size validations for state channels and burns

--- src/transactions/v1/blockchain_txn_token_burn_v1.erl
+++ src/transactions/v1/blockchain_txn_token_burn_v1.erl
@@ -104,17 +104,22 @@ is_valid(Txn, Chain) ->
     PubKey = libp2p_crypto:bin_to_pubkey(Payer),
     BaseTxn = Txn#blockchain_txn_token_burn_v1_pb{signature = <<>>},
     EncodedTxn = blockchain_txn_token_burn_v1_pb:encode_msg(BaseTxn),
-    case libp2p_crypto:verify(EncodedTxn, Signature, PubKey) of
-        false ->
-            {error, bad_signature};
-        true ->
-            case blockchain_ledger_v1:config(?token_burn_exchange_rate, Ledger) of
-                {error, _Reason}=Error ->
-                    Error;
-                {ok, _Rate} ->
-                    Amount = ?MODULE:amount(Txn),
-                    blockchain_ledger_v1:check_balance(Payer, Amount, Ledger)
-            end
+    case blockchain_txn:validate_fields([{{payee, ?MODULE:payee(Txn)}, {address, libp2p}}]) of
+        ok ->
+            case libp2p_crypto:verify(EncodedTxn, Signature, PubKey) of
+                false ->
+                    {error, bad_signature};
+                true ->
+                    case blockchain_ledger_v1:config(?token_burn_exchange_rate, Ledger) of
+                        {error, _Reason}=Error ->
+                            Error;
+                        {ok, _Rate} ->
+                            Amount = ?MODULE:amount(Txn),
+                            blockchain_ledger_v1:check_balance(Payer, Amount, Ledger)
+                    end
+            end;
+        Error ->
+            Error
     end.
 
 
