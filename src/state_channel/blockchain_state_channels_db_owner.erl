b0b4fb7494d396bc7697adb10c3e9349f43a5ff7
Mark Allen <mrallen1@yahoo.com>
2020-08-12 17:44:45 -0500

    Serialize state channel writes to disk

--- src/state_channel/blockchain_state_channels_db_owner.erl
+++ src/state_channel/blockchain_state_channels_db_owner.erl
@@ -5,6 +5,8 @@
 %% * This process is started first in the state_channels supervision tree
 %% * SC client/server will get the db reference from here when they init
 %% * This process also traps exits and closes rocksdb (if need be)
+%% * This process is responsible for serializing SC updates to disk in a
+%%   batch write each write interval (currently 1000 millis)
 %%
 %% @end
 %%%-------------------------------------------------------------------
@@ -16,7 +18,9 @@
 -export([start_link/1,
          db/0,
          sc_servers_cf/0,
-         sc_clients_cf/0
+         sc_clients_cf/0,
+         write/2,
+         gc/1
         ]).
 
 %% gen_server exports
@@ -30,12 +34,16 @@
 ]).
 
 -define(DB_FILE, "state_channels.db").
+-define(WRITE_INTERVAL, 1000). % milliseconds
+-define(TICK, '__sc_write_tick').
 
 -record(state, {
           db :: rocksdb:db_handle(),
           default :: rocksdb:cf_handle(),
           sc_servers_cf :: rocksdb:cf_handle(),
-          sc_clients_cf :: rocksdb:cf_handle()
+          sc_clients_cf :: rocksdb:cf_handle(),
+          tref :: reference(),
+          pending = #{} :: maps:map()
          }).
 
 %% api functions
@@ -54,6 +62,15 @@ sc_servers_cf() ->
 sc_clients_cf() ->
     gen_server:call(?MODULE, sc_clients_cf).
 
+-spec write( SC :: blockchain_state_channel_v1:state_channel(),
+             Skewed :: skewed:skewed()) -> ok.
+write(SC, Skewed) ->
+    gen_server:call(?MODULE, {write, SC, Skewed}, infinity).
+
+-spec gc( [ blockchain_state_channel_v1:id() ] ) -> ok.
+gc(IDs) ->
+    gen_server:call(?MODULE, {gc, IDs}, infinity).
+
 %% gen_server callbacks
 init(Args) ->
     lager:info("~p init with ~p", [?MODULE, Args]),
@@ -61,7 +78,9 @@ init(Args) ->
     BaseDir = maps:get(base_dir, Args),
     CFs = maps:get(cfs, Args, ["default", "sc_servers_cf", "sc_clients_cf"]),
     {ok, DB, [DefaultCF, SCServersCF, SCClientsCF]} = open_db(BaseDir, CFs),
-    {ok, #state{db=DB, default=DefaultCF, sc_servers_cf=SCServersCF, sc_clients_cf=SCClientsCF}}.
+    Tref = schedule_next_tick(),
+    {ok, #state{db=DB, default=DefaultCF, sc_servers_cf=SCServersCF,
+                sc_clients_cf=SCClientsCF, tref=Tref}}.
 
 handle_call(db, _From, #state{db=DB}=State) ->
     {reply, DB, State};
@@ -69,6 +88,13 @@ handle_call(sc_clients_cf, _From, #state{sc_clients_cf=CF}=State) ->
     {reply, CF, State};
 handle_call(sc_servers_cf, _From, #state{sc_servers_cf=CF}=State) ->
     {reply, CF, State};
+handle_call({gc, IDs}, _From, #state{pending=P}=State)->
+    {reply, ok, State#state{pending=maps:without(IDs, P)}};
+handle_call({write, SC, Skewed}, _From, #state{pending=P}=State) ->
+    SCID = blockchain_state_channel_v1:id(SC),
+    %% defer encoding until write time
+    NewP = maps:put(SCID, {SC, Skewed}, P),
+    {reply, ok, State#state{pending=NewP}};
 handle_call(_Msg, _From, State) ->
     lager:warning("rcvd unknown call msg: ~p from: ~p", [_Msg, _From]),
     {reply, ok, State}.
@@ -81,6 +107,15 @@ handle_info({'EXIT', _From, _Reason} , #state{db=DB}=State) ->
     lager:info("EXIT because: ~p, closing rocks: ~p", [_Reason, DB]),
     ok = rocksdb:close(DB),
     {stop, db_owner_exit, State};
+handle_info(?TICK, #state{pending=P}=State) when map_size(P) == 0 ->
+    lager:debug("No pending writes this tick"),
+    Tref = schedule_next_tick(),
+    {noreply, State#state{tref=Tref}};
+handle_info(?TICK, #state{pending=P, db=DB}=State) ->
+    lager:info("~p pending writes this tick", [map_size(P)]),
+    ok = handle_batch_write(DB, P),
+    Tref = schedule_next_tick(),
+    {noreply, State#state{tref=Tref, pending=#{}}};
 handle_info(_Msg, State) ->
     lager:warning("rcvd unknown info msg: ~p", [_Msg]),
     {noreply, State}.
@@ -88,7 +123,11 @@ handle_info(_Msg, State) ->
 code_change(_OldVsn, State, _Extra) ->
     {ok, State}.
 
-terminate(_Reason, #state{db=DB}) ->
+terminate(_Reason, #state{db=DB, pending=P}) when map_size(P) == 0 ->
+    ok = rocksdb:close(DB),
+    ok;
+terminate(_Reason, #state{db=DB, pending=P}) ->
+    ok = handle_batch_write(DB, P),
     ok = rocksdb:close(DB),
     ok.
 
@@ -124,3 +163,18 @@ open_db(Dir, CFNames) ->
             L3 = L1 ++ L2,
             {ok, DB, [proplists:get_value(X, L3) || X <- CFNames]}
     end.
+
+schedule_next_tick() ->
+    erlang:send_after(?WRITE_INTERVAL, self(), ?TICK).
+
+handle_batch_write(DB, P) ->
+    {ok, Batch} = rocksdb:batch(),
+    ok = maps:fold(fun(SCID, {SC, Skewed}, Acc) ->
+                      Bin = term_to_binary({blockchain_state_channel_v1:encode(SC),
+                                            Skewed}),
+                      ok = rocksdb:batch_put(Batch, SCID, Bin),
+                      Acc
+              end, ok, P),
+    Res = rocksdb:write_batch(DB, Batch, []),
+    ok = rocksdb:release_batch(Batch),
+    Res.
