87a9f84ae95b6c022168db38df4294f4ebf42811
Robert Keith <robertfkeith@me.com>
2020-09-10 09:11:13 -0700

    dBm is expressed as negative value. Subtracting from transmit power produces negative, but is not the actual FSPL value.

--- src/blockchain_utils.erl
+++ src/blockchain_utils.erl
@@ -245,7 +245,7 @@ free_space_path_loss(Loc1, Loc2) ->
     %% TODO support variable Dt,Dr values for better FSPL values
     %% FSPL = 10log_10(Dt*Dr*((4*pi*f*d)/(c))^2)
     %%
-    ?TRANSMIT_POWER - (10*math:log10((1.8*1.8)*math:pow((4*math:pi()*(?FREQUENCY*100000)*(Distance*100000))/(299792458), 2))).
+    -(10*math:log10((1.8*1.8)*math:pow((4*math:pi()*(?FREQUENCY*100000)*(Distance*100000))/(299792458), 2))).
 
 -spec vars_binary_keys_to_atoms(map()) -> map().
 vars_binary_keys_to_atoms(Vars) ->
