d92073e3df9ba096f03e19ce025c10833f6d6e65
Eric Heilman <>
2021-02-08 06:57:26 -0600

    no message

--- src/transactions/v1/blockchain_txn_rewards_v1.erl
+++ src/transactions/v1/blockchain_txn_rewards_v1.erl
@@ -134,7 +134,7 @@ absorb(Txn, Chain) ->
     AccRewards = lists:foldl(
         fun(Reward, Acc) ->
             Account = blockchain_txn_reward_v1:account(Reward),
-            Amount = (blockchain_txn_reward_v1:amount(Reward),
+            Amount = blockchain_txn_reward_v1:amount(Reward),
          %%   Gateway = blockchain_txn_reward_v1:gateway(Reward),
          %%   Split = blockchain_ledger_gateway_v2:get_split(Gateway, Account),
          %%   Amount = (blockchain_txn_reward_v1:amount(Reward) * Split) / 100,
