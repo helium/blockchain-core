d33ae61241c97a2d11bc6908a499a90c84422b34
Steven Yuan <syuan100@gmail.com>
2021-04-19 22:31:03 -0700

    Add test to check for env disabled

--- test/blockchain_implicit_burn_SUITE.erl
+++ test/blockchain_implicit_burn_SUITE.erl
@@ -7,12 +7,14 @@
 -export([all/0, init_per_testcase/2, end_per_testcase/2]).
 
 -export([
-         basic_test/1
+         enable_implicit_burn_test/1,
+         disabled_implicit_burn_test/1
         ]).
 
 all() ->
     [
-     basic_test
+     enable_implicit_burn_test,
+     disabled_implicit_burn_test
     ].
 
 -define(MAX_PAYMENTS, 20).
@@ -27,8 +29,6 @@ init_per_testcase(TestCase, Config) ->
     {ok, Sup, {PrivKey, PubKey}, Opts} = test_utils:init(?config(base_dir, Config0)),
 
     ExtraVars = extra_vars(TestCase),
-
-    ok = application:set_env(blockchain, store_implicit_burns, true),
 
     {ok, GenesisMembers, _GenesisBlock, ConsensusMembers, Keys} =
         test_utils:init_chain(Balance, {PrivKey, PubKey}, true, ExtraVars),
@@ -80,12 +80,14 @@ end_per_testcase(_, Config) ->
 %% TEST CASES
 %%--------------------------------------------------------------------
 
-basic_test(Config) ->
+enable_implicit_burn_test(Config) ->
     BaseDir = ?config(base_dir, Config),
     ConsensusMembers = ?config(consensus_members, Config),
-    Balance = ?config(balance, Config),
     BaseDir = ?config(base_dir, Config),
     Chain = ?config(chain, Config),
+
+    %% Set env to store implicit burn %%
+    ok = application:set_env(blockchain, store_implicit_burns, true),
 
     %% Test a payment transaction, add a block and check balances
     [_, {Payer, {_, PayerPrivKey, _}}|_] = ConsensusMembers,
@@ -106,25 +108,46 @@ basic_test(Config) ->
     {ok, Block} = test_utils:create_block(ConsensusMembers, [SignedTx]),
     _ = blockchain_gossip_handler:add_block(Block, Chain, self(), blockchain_swarm:swarm()),
 
-    ?assertEqual({ok, blockchain_block:hash_block(Block)}, blockchain:head_hash(Chain)),
-    ?assertEqual({ok, Block}, blockchain:head_block(Chain)),
-    ?assertEqual({ok, 2}, blockchain:height(Chain)),
-
-    ?assertEqual({ok, Block}, blockchain:get_block(2, Chain)),
-
-    Ledger = blockchain:ledger(Chain),
-
     TxHash = blockchain_txn:hash(SignedTx),
 
     %% Check that there is an implicit burn
     {ok, {implicit_burn, _, _}} = blockchain:get_implicit_burn(TxHash, Chain),
     ct:pal("implicit burn: ~p", [blockchain:get_implicit_burn(TxHash, Chain)]),
+    ok.
 
-    {ok, NewEntry0} = blockchain_ledger_v1:find_entry(Recipient, Ledger),
-    ?assertEqual(Balance + Amount, blockchain_ledger_entry_v1:balance(NewEntry0)),
+disabled_implicit_burn_test(Config) ->
+    BaseDir = ?config(base_dir, Config),
+    ConsensusMembers = ?config(consensus_members, Config),
+    BaseDir = ?config(base_dir, Config),
+    Chain = ?config(chain, Config),
 
-    {ok, NewEntry1} = blockchain_ledger_v1:find_entry(Payer, Ledger),
-    ?assertEqual(Balance - Amount, blockchain_ledger_entry_v1:balance(NewEntry1)),
+    %% Set env to store implicit burn %%
+    ok = application:set_env(blockchain, store_implicit_burns, false),
+
+    %% Test a payment transaction, add a block and check balances
+    [_, {Payer, {_, PayerPrivKey, _}}|_] = ConsensusMembers,
+
+    %% Create a payment to a single payee
+    Recipient = blockchain_swarm:pubkey_bin(),
+    Amount = 2500,
+    Payment1 = blockchain_payment_v2:new(Recipient, Amount),
+
+    Tx0 = blockchain_txn_payment_v2:new(Payer, [Payment1], 1),
+    Fee = blockchain_txn_payment_v2:calculate_fee(Tx0, Chain),
+    Tx1 = blockchain_txn_payment_v2:fee(Tx0, Fee),
+    SigFun = libp2p_crypto:mk_sig_fun(PayerPrivKey),
+    SignedTx = blockchain_txn_payment_v2:sign(Tx1, SigFun),
+
+    ct:pal("~s", [blockchain_txn:print(SignedTx)]),
+
+    {ok, Block} = test_utils:create_block(ConsensusMembers, [SignedTx]),
+    _ = blockchain_gossip_handler:add_block(Block, Chain, self(), blockchain_swarm:swarm()),
+
+    TxHash = blockchain_txn:hash(SignedTx),
+
+    %% Check that there is NO implicit burn
+    {error, not_found} = blockchain:get_implicit_burn(TxHash, Chain),
+    ct:pal("Confirmed that implicit burn has not been stored for ~p", [TxHash]),
     ok.
 
 %%--------------------------------------------------------------------
