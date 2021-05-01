bf98103c131f4b7b2b3acc186fe90a04ccd661d8
Rahul Garg <rg@vihu.dev>
2021-04-30 17:58:34 -0700

    Initial support to specify regional params
    This adds support for encapsulating regional parameter information as chain variables. Essentially each of these regional parameter value will be a KNOWN binary blob.
    
    - Only works for US915
    - Add test for ser/de
    - Add param_v1 module
    - Switch proto
    - Add channel param verification

--- include/blockchain.hrl
+++ include/blockchain.hrl
@@ -12,7 +12,6 @@
 
 -define(SNAPSHOT_PROTOCOL, "blockchain_snapshot/1.0.0").
 
-
 -define(TX_PROTOCOL, "blockchain_txn/1.0.0").
 -define(LOC_ASSERTION_PROTOCOL, "loc_assertion/1.0.0").
 -define(STATE_CHANNEL_PROTOCOL_V1, "state_channel/1.0.0").
@@ -25,13 +24,15 @@
 % Misc
 -define(EVT_MGR, blockchain_event_mgr).
 
--define(BC_UPGRADE_NAMES, [<<"gateway_v2">>,
-                           <<"hex_targets">>,
-                           <<"gateway_oui">>,
-                           <<"h3dex">>,
-                           <<"h3dex2">>,
-                           <<"gateway_lg3">>,
-                           <<"clear_witnesses">>]).
+-define(BC_UPGRADE_NAMES, [
+    <<"gateway_v2">>,
+    <<"hex_targets">>,
+    <<"gateway_oui">>,
+    <<"h3dex">>,
+    <<"h3dex2">>,
+    <<"gateway_lg3">>,
+    <<"clear_witnesses">>
+]).
 
 -define(bones(HNT), HNT * 100000000).
 
@@ -47,4 +48,55 @@
     "kr920",
     "ru864",
     "us915"
+]).
+
+-define(REGION_PARAMS_US915, [
+    [
+        {<<"channel_frequency">>, 903900000},
+        {<<"bandwidth">>, 125},
+        {<<"max_power">>, 36},
+        {<<"spreading">>, [<<"sf10">>, <<"sf9">>, <<"sf8">>, <<"sf7">>]}
+    ],
+    [
+        {<<"channel_frequency">>, 904100000},
+        {<<"bandwidth">>, 125},
+        {<<"max_power">>, 36},
+        {<<"spreading">>, [<<"sf10">>, <<"sf9">>, <<"sf8">>, <<"sf7">>]}
+    ],
+    [
+        {<<"channel_frequency">>, 904300000},
+        {<<"bandwidth">>, 125},
+        {<<"max_power">>, 36},
+        {<<"spreading">>, [<<"sf10">>, <<"sf9">>, <<"sf8">>, <<"sf7">>]}
+    ],
+    [
+        {<<"channel_frequency">>, 904500000},
+        {<<"bandwidth">>, 125},
+        {<<"max_power">>, 36},
+        {<<"spreading">>, [<<"sf10">>, <<"sf9">>, <<"sf8">>, <<"sf7">>]}
+    ],
+    [
+        {<<"channel_frequency">>, 904700000},
+        {<<"bandwidth">>, 125},
+        {<<"max_power">>, 36},
+        {<<"spreading">>, [<<"sf10">>, <<"sf9">>, <<"sf8">>, <<"sf7">>]}
+    ],
+    [
+        {<<"channel_frequency">>, 904900000},
+        {<<"bandwidth">>, 125},
+        {<<"max_power">>, 36},
+        {<<"spreading">>, [<<"sf10">>, <<"sf9">>, <<"sf8">>, <<"sf7">>]}
+    ],
+    [
+        {<<"channel_frequency">>, 905100000},
+        {<<"bandwidth">>, 125},
+        {<<"max_power">>, 36},
+        {<<"spreading">>, [<<"sf10">>, <<"sf9">>, <<"sf8">>, <<"sf7">>]}
+    ],
+    [
+        {<<"channel_frequency">>, 905300000},
+        {<<"bandwidth">>, 125},
+        {<<"max_power">>, 36},
+        {<<"spreading">>, [<<"sf10">>, <<"sf9">>, <<"sf8">>, <<"sf7">>]}
+    ]
 ]).
