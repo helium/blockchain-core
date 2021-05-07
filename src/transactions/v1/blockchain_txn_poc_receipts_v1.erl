bbfb1f0c7a9f97901480e65a961b8d7d83cecea9
Rahul Garg <rg@vihu.dev>
2021-05-07 14:48:33 -0700

    And also in two more places

--- src/transactions/v1/blockchain_txn_poc_receipts_v1.erl
+++ src/transactions/v1/blockchain_txn_poc_receipts_v1.erl
@@ -1151,8 +1151,11 @@ valid_receipt(PreviousElement, Element, Channel, Ledger) ->
                          _ ->
                              false
                      end,
+
+            SameRegion = is_same_region(Ledger, SourceLoc, DestinationLoc),
+
             try h3:grid_distance(SourceParentIndex, DestinationParentIndex) >= ExclusionCells of
-                Dist when Dist >= ExclusionCells andalso not TooFar ->
+                Dist when Dist >= ExclusionCells andalso SameRegion andalso not TooFar ->
                     RSSI = blockchain_poc_receipt_v1:signal(Receipt),
                     SNR = blockchain_poc_receipt_v1:snr(Receipt),
                     Freq = blockchain_poc_receipt_v1:frequency(Receipt),
@@ -1346,8 +1349,11 @@ tagged_witnesses(Element, Channel, Ledger) ->
                                       _ ->
                                           false
                                   end,
+
+                         SameRegion = is_same_region(Ledger, SourceLoc, DestinationLoc),
+
                          try h3:grid_distance(SourceParentIndex, DestinationParentIndex) of
-                             Dist when Dist >= ExclusionCells andalso not TooFar ->
+                             Dist when Dist >= ExclusionCells andalso SameRegion andalso not TooFar ->
                                  RSSI = blockchain_poc_witness_v1:signal(Witness),
                                  SNR = blockchain_poc_witness_v1:snr(Witness),
                                  Freq = blockchain_poc_witness_v1:frequency(Witness),
