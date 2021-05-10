8bfda12e8ffb330026e67fc7e541ff5dce066e88
Rahul Garg <rg@vihu.dev>
2021-05-10 10:05:37 -0700

    Add remaining region param support

--- src/region/blockchain_region_params_v1.erl
+++ src/region/blockchain_region_params_v1.erl
@@ -17,6 +17,15 @@
 -export([
     serialized_us915/0,
     serialized_eu868/0,
+    serialized_au915/0,
+    serialized_as923_1/0,
+    serialized_as923_2/0,
+    serialized_as923_3/0,
+    serialized_ru864/0,
+    serialized_cn470/0,
+    serialized_in865/0,
+    serialized_kr920/0,
+    serialized_eu433/0,
 
     new/1,
     fetch/1,
@@ -38,12 +47,75 @@ serialized_us915() ->
 serialized_eu868() ->
     serialize(fetch(eu868)).
 
+-spec serialized_au915() -> binary().
+serialized_au915() ->
+    serialize(fetch(au915)).
+
+-spec serialized_as923_1() -> binary().
+serialized_as923_1() ->
+    serialize(fetch(as923_1)).
+
+-spec serialized_as923_2() -> binary().
+serialized_as923_2() ->
+    serialize(fetch(as923_2)).
+
+-spec serialized_as923_3() -> binary().
+serialized_as923_3() ->
+    serialize(fetch(as923_3)).
+
+-spec serialized_ru864() -> binary().
+serialized_ru864() ->
+    serialize(fetch(ru864)).
+
+-spec serialized_cn470() -> binary().
+serialized_cn470() ->
+    serialize(fetch(cn470)).
+
+-spec serialized_in865() -> binary().
+serialized_in865() ->
+    serialize(fetch(in865)).
+
+-spec serialized_kr920() -> binary().
+serialized_kr920() ->
+    serialize(fetch(kr920)).
+
+-spec serialized_eu433() -> binary().
+serialized_eu433() ->
+    serialize(fetch(eu433)).
+
 -spec fetch(atom()) -> region_params_v1().
 fetch(us915) ->
     Params = make_params(?REGION_PARAMS_US915),
     new(Params);
 fetch(eu868) ->
     Params = make_params(?REGION_PARAMS_EU868),
+    new(Params);
+fetch(au915) ->
+    Params = make_params(?REGION_PARAMS_AU915),
+    new(Params);
+fetch(as923_1) ->
+    Params = make_params(?REGION_PARAMS_AS923_1),
+    new(Params);
+fetch(as923_2) ->
+    Params = make_params(?REGION_PARAMS_AS923_2),
+    new(Params);
+fetch(as923_3) ->
+    Params = make_params(?REGION_PARAMS_AS923_3),
+    new(Params);
+fetch(ru864) ->
+    Params = make_params(?REGION_PARAMS_RU864),
+    new(Params);
+fetch(cn470) ->
+    Params = make_params(?REGION_PARAMS_CN470),
+    new(Params);
+fetch(in865) ->
+    Params = make_params(?REGION_PARAMS_IN865),
+    new(Params);
+fetch(kr920) ->
+    Params = make_params(?REGION_PARAMS_KR920),
+    new(Params);
+fetch(eu433) ->
+    Params = make_params(?REGION_PARAMS_EU433),
     new(Params).
 
 -spec new(RegionParams :: [blockchain_region_param_v1:region_param_v1()]) -> region_params_v1().
@@ -78,4 +150,3 @@ construct_param(P) ->
     MaxEIRP = proplists:get_value(<<"max_eirp">>, P),
     Spreading = blockchain_region_spreading_v1:new(proplists:get_value(<<"spreading">>, P)),
     blockchain_region_param_v1:new(CF, BW, MaxEIRP, Spreading).
-
