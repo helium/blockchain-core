111a5bbcc35c0e3657808358102529b5bfe9062e
Andrew Thompson <andrew@hijacked.us>
2021-05-11 15:22:27 -0700

    Bah

--- src/ledger/v1/blockchain_ledger_v1.erl
+++ src/ledger/v1/blockchain_ledger_v1.erl
@@ -3078,10 +3078,15 @@ close(#ledger_v1{db=DB}=L) ->
     end.
 
 
-compact(#ledger_v1{db=_DB, active=_Active, delayed=_Delayed}) ->
+compact(#ledger_v1{db=DB, active=Active, delayed=Delayed}) ->
     %rocksdb:compact_range(DB, undefined, undefined, []),
-    %compact_ledger(DB, Active),
-    %compact_ledger(DB, Delayed),
+    case false of
+        true ->
+            compact_ledger(DB, Active),
+            compact_ledger(DB, Delayed);
+        false ->
+            ok
+    end,
     ok.
 
 -spec state_channel_key(libp2p_crypto:pubkey_bin(), binary()) -> binary().
