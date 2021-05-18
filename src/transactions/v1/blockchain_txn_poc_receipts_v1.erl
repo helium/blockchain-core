f3af9caa871ce73de23003137e73cf5f96a9fc50
Rahul Garg <3199183+vihu@users.noreply.github.com>
2021-05-18 16:55:37 -0700

    Update src/transactions/v1/blockchain_txn_poc_receipts_v1.erl
    Co-authored-by: Michael Jeffrey <michaeldjeffrey@gmail.com>

--- src/transactions/v1/blockchain_txn_poc_receipts_v1.erl
+++ src/transactions/v1/blockchain_txn_poc_receipts_v1.erl
@@ -1154,7 +1154,7 @@ valid_receipt(PreviousElement, Element, Channel, Ledger) ->
 
             SameRegion = is_same_region(Ledger, SourceLoc, DestinationLoc),
 
-            try h3:grid_distance(SourceParentIndex, DestinationParentIndex) >= ExclusionCells of
+            try h3:grid_distance(SourceParentIndex, DestinationParentIndex) of
                 Dist when Dist >= ExclusionCells andalso SameRegion andalso not TooFar ->
                     RSSI = blockchain_poc_receipt_v1:signal(Receipt),
                     SNR = blockchain_poc_receipt_v1:snr(Receipt),
@@ -1695,4 +1695,3 @@ to_json_test() ->
 
 
 -endif.
-
