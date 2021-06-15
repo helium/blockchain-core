c09c2c2e1999ba37ac6a61f8b72d64fd3c0cf769
Rahul Garg <rg@vihu.dev>
2021-06-15 12:56:52 -0700

    Remove internal param mapping, enfore region_*

--- src/region/blockchain_region_params_v1.erl
+++ src/region/blockchain_region_params_v1.erl
@@ -8,9 +8,6 @@
 %% TODO
 %% -behavior(blockchain_json).
 
--include("blockchain_json.hrl").
--include("blockchain_vars.hrl").
-
 -include_lib("helium_proto/include/blockchain_region_param_v1_pb.hrl").
 
 -export([
@@ -23,36 +20,21 @@
 
 -type region_params_v1() :: #blockchain_region_params_v1_pb{}.
 
--define(REGION_PARAM_MAP, #{
-    us915 => ?region_params_us915,
-    eu868 => ?region_params_eu868,
-    as923_1 => ?region_params_as923_1,
-    as923_2 => ?region_params_as923_2,
-    as923_3 => ?region_params_as923_3,
-    au915 => ?region_params_au915,
-    ru864 => ?region_params_ru864,
-    cn470 => ?region_params_cn470,
-    in865 => ?region_params_in865,
-    kr920 => ?region_params_kr920,
-    eu433 => ?region_params_eu433
-}).
-
 -export_type([region_params_v1/0]).
 
 %%--------------------------------------------------------------------
 %% api
 %%--------------------------------------------------------------------
 
--spec for_region(Region :: atom(), Ledger :: blockchain_ledger_v1:ledger()) ->
+-spec for_region(RegionVar :: atom(), Ledger :: blockchain_ledger_v1:ledger()) ->
     {ok, [blockchain_region_param_v1:region_param_v1()]} | {error, any()}.
-for_region(Region, Ledger) ->
-    Var = maps:get(Region, ?REGION_PARAM_MAP),
-    case blockchain:config(Var, Ledger) of
+for_region(RegionVar, Ledger) ->
+    case blockchain:config(RegionVar, Ledger) of
         {ok, Bin} ->
             Deser = deserialize(Bin),
             {ok, region_params(Deser)};
         _ ->
-            {error, {not_set, Var}}
+            {error, {not_set, RegionVar}}
     end.
 
 -spec new(RegionParams :: [blockchain_region_param_v1:region_param_v1()]) -> region_params_v1().
