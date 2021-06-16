14c193dc77d0f4aee935fe8a3995ed499599c997
Rahul Garg <rg@vihu.dev>
2021-06-16 15:00:10 -0700

    Add support for tagged spreading
    - Fix test cases
    - Fix [de]serialization
    - Remove CN779
    - Update test header and test serialized region param values
    - Switch proto branch

--- src/region/blockchain_region_spreading_v1.erl
+++ src/region/blockchain_region_spreading_v1.erl
@@ -14,28 +14,39 @@
 -include_lib("helium_proto/include/blockchain_region_param_v1_pb.hrl").
 
 -export([
-    new/1
+    new/1,
+
+    max_packet_size/1,
+    region_spreading/1
 ]).
 
 -type region_spreading_v1() :: #blockchain_region_spreading_v1_pb{}.
+-type tagged_spreading() :: #tagged_spreading_pb{}.
 
 %%--------------------------------------------------------------------
 %% api
 %%--------------------------------------------------------------------
 
--spec new(Spreading :: [binary()]) -> region_spreading_v1().
-new(Spreading) ->
-    #blockchain_region_spreading_v1_pb{region_spreading = [to_spreading(I) || I <- Spreading]}.
+-spec new(Spreads :: [{non_neg_integer(), atom()}]) -> region_spreading_v1().
+new(Spreads) ->
+    Tags = [new_tagged_spread(Size, Spread) || {Size, Spread} <- Spreads],
+    #blockchain_region_spreading_v1_pb{tagged_spreading = Tags}.
+
+-spec max_packet_size(TaggedSpreading :: tagged_spreading()) -> non_neg_integer().
+max_packet_size(TaggedSpreading) ->
+    TaggedSpreading#tagged_spreading_pb.max_packet_size.
+
+-spec region_spreading(TaggedSpreading :: tagged_spreading()) -> undefined | atom().
+region_spreading(TaggedSpreading) ->
+    TaggedSpreading#tagged_spreading_pb.region_spreading.
 
 %%--------------------------------------------------------------------
 %% helpers
 %%--------------------------------------------------------------------
 
--spec to_spreading(binary()) -> atom().
-to_spreading(<<"sf7">>) -> 'SF7';
-to_spreading(<<"sf8">>) -> 'SF8';
-to_spreading(<<"sf9">>) -> 'SF9';
-to_spreading(<<"sf10">>) -> 'SF10';
-to_spreading(<<"sf11">>) -> 'SF11';
-to_spreading(<<"sf12">>) -> 'SF12';
-to_spreading(_) -> undefined.
+-spec new_tagged_spread(
+    Size :: undefined | non_neg_integer(),
+    Spreading :: undefined | atom()
+) -> tagged_spreading().
+new_tagged_spread(Size, Spreading) ->
+    #tagged_spreading_pb{max_packet_size = Size, region_spreading = Spreading}.
