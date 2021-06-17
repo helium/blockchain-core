9ebeca6d4da2699fb0a54a96c50729e36cb61484
macpie <pierre@helium.com>
2021-06-17 16:24:16 -0700

    improve get_sc_max_actors

--- src/state_channel/blockchain_state_channels_server.erl
+++ src/state_channel/blockchain_state_channels_server.erl
@@ -450,11 +450,14 @@ get_sc_max_actors(Chain) ->
     MaxActorsAllowed = blockchain_state_channel_v1:max_actors_allowed(Ledger),
     case application:get_env(blockchain, sc_max_actors, MaxActorsAllowed) of
         Str when is_list(Str) ->
-            case erlang:list_to_integer(Str) of
+            try erlang:list_to_integer(Str) of
                 TooHigh when TooHigh > MaxActorsAllowed ->
                     MaxActorsAllowed;
                 Max ->
                     Max
+            catch What:Why ->
+                lager:info("failed to convert sc_max_actors to int ~p", [{What, Why}]),
+                MaxActorsAllowed
             end;
         TooHigh when TooHigh > MaxActorsAllowed ->
             MaxActorsAllowed;
