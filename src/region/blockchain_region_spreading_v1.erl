93191107e5e1b67476c8e45146c6dbad79027c65
Rahul Garg <rg@vihu.dev>
2021-06-16 18:27:10 -0700

    Add spreading selection

--- src/region/blockchain_region_spreading_v1.erl
+++ src/region/blockchain_region_spreading_v1.erl
@@ -7,15 +7,13 @@
 
 %% TODO
 %% -behavior(blockchain_json).
-
--include("blockchain_json.hrl").
--include("blockchain_vars.hrl").
-
 -include_lib("helium_proto/include/blockchain_region_param_v1_pb.hrl").
 
 -export([
     new/1,
+    tagged_spreading/1,
 
+    select_spreading/2,
     max_packet_size/1,
     region_spreading/1
 ]).
@@ -32,6 +30,10 @@ new(Spreads) ->
     Tags = [new_tagged_spread(Size, Spread) || {Size, Spread} <- Spreads],
     #blockchain_region_spreading_v1_pb{tagged_spreading = Tags}.
 
+-spec tagged_spreading(Spreading :: region_spreading_v1()) -> [tagged_spreading()].
+tagged_spreading(Spreading) ->
+    Spreading#blockchain_region_spreading_v1_pb.tagged_spreading.
+
 -spec max_packet_size(TaggedSpreading :: tagged_spreading()) -> non_neg_integer().
 max_packet_size(TaggedSpreading) ->
     TaggedSpreading#tagged_spreading_pb.max_packet_size.
@@ -39,6 +41,22 @@ max_packet_size(TaggedSpreading) ->
 -spec region_spreading(TaggedSpreading :: tagged_spreading()) -> undefined | atom().
 region_spreading(TaggedSpreading) ->
     TaggedSpreading#tagged_spreading_pb.region_spreading.
+
+-spec select_spreading(
+    TaggedSpreading :: [tagged_spreading()],
+    PacketSize :: non_neg_integer()
+) -> {ok, atom()} | {error, any()}.
+select_spreading(TaggedSpreading, PacketSize) ->
+    %% FIXME: This is not quite right...
+    FilterFun = fun(Tagged) ->
+        max_packet_size(Tagged) >= PacketSize
+    end,
+    case lists:filter(FilterFun, TaggedSpreading) of
+        [] -> {error, unable_to_get_spreading};
+        Thing ->
+            Tag = hd(Thing),
+            {ok, region_spreading(Tag)}
+    end.
 
 %%--------------------------------------------------------------------
 %% helpers
