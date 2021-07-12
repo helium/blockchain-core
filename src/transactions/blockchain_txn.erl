906364976353640fdf19de326d23d9ab2177c479
Evan Vigil-McClanahan <evan@helium.com>
2021-07-12 12:52:18 -0700

    force clean witnesses, don't allow them to be added, disable gc

--- src/transactions/blockchain_txn.erl
+++ src/transactions/blockchain_txn.erl
@@ -833,11 +833,11 @@ absorb_aux(Block0, Chain0) ->
 plain_absorb_(Block, Chain0) ->
     case ?MODULE:absorb_block(Block, Chain0) of
         {ok, _} ->
-            Hash = blockchain_block:hash_block(Block),
+            %% Hash = blockchain_block:hash_block(Block),
             Ledger0 = blockchain:ledger(Chain0),
             ok = blockchain_ledger_v1:maybe_gc_pocs(Chain0, Ledger0),
             ok = blockchain_ledger_v1:maybe_gc_scs(Chain0, Ledger0),
-            ok = blockchain_ledger_v1:refresh_gateway_witnesses(Hash, Ledger0),
+            %% ok = blockchain_ledger_v1:refresh_gateway_witnesses(Hash, Ledger0),
             ok = blockchain_ledger_v1:maybe_recalc_price(Chain0, Ledger0),
             ok;
         Error ->
