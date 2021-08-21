bd82343bfc803713aa3828c97e774ed4e4e4dc67
Rahul Garg <rg@vihu.dev>
2021-08-20 17:17:18 -0700

    Add cache for h3 region lookups

--- src/region/blockchain_region_v1.erl
+++ src/region/blockchain_region_v1.erl
@@ -13,6 +13,14 @@
 
 -export_type([atom/0, regions/0]).
 
+-define(H3_IN_REGION_CACHE, h3_in_region).
+-define(H3_TO_REGION_CACHE, h3_to_region).
+
+%% 24 hours in seconds
+-define(H3_TO_REGION_CACHE_TIMEOUT, 60 * 60 * 24).
+%% 24 hours in seconds
+-define(H3_IN_REGION_CACHE_TIMEOUT, 60 * 60 * 24).
+
 %%--------------------------------------------------------------------
 %% api
 %%--------------------------------------------------------------------
@@ -30,12 +38,15 @@ get_all_regions(Ledger) ->
 -spec h3_to_region(H3 :: h3:h3_index(), Ledger :: blockchain_ledger_v1:ledger()) ->
     {ok, atom()} | {error, any()}.
 h3_to_region(H3, Ledger) ->
-    case get_all_regions(Ledger) of
-        {ok, Regions} ->
-            region_(Regions, H3, Ledger);
-        E ->
-            E
-    end.
+    {ok, VarsNonce} = blockchain_ledger_v1:vars_nonce(Ledger),
+    e2qc:cache(
+        ?H3_TO_REGION_CACHE,
+        {VarsNonce, H3},
+        ?H3_TO_REGION_CACHE_TIMEOUT,
+        fun() ->
+            h3_to_region_(H3, Ledger)
+        end
+    ).
 
 -spec h3_in_region(
     H3 :: h3:h3_index(),
@@ -43,6 +54,50 @@ h3_to_region(H3, Ledger) ->
     Ledger :: blockchain_ledger_v1:ledger()
 ) -> boolean() | {error, any()}.
 h3_in_region(H3, RegionVar, Ledger) ->
+    {ok, VarsNonce} = blockchain_ledger_v1:vars_nonce(Ledger),
+    e2qc:cache(
+        ?H3_IN_REGION_CACHE,
+        {VarsNonce, H3, RegionVar},
+        ?H3_IN_REGION_CACHE_TIMEOUT,
+        fun() ->
+            h3_in_region_(H3, RegionVar, Ledger)
+        end
+    ).
+
+%%--------------------------------------------------------------------
+%% helpers
+%%--------------------------------------------------------------------
+-spec region_(
+    Regions :: regions(),
+    H3 :: h3:h3_index(),
+    Ledger :: blockchain_ledger_v1:ledger()
+) ->
+    {ok, atom()} | {error, any()}.
+region_([], H3, _Ledger) ->
+    {error, {unknown_region, H3}};
+region_([ToCheck | Remaining], H3, Ledger) ->
+    case h3_in_region(H3, ToCheck, Ledger) of
+        {error, _} = Error -> Error;
+        false -> region_(Remaining, H3, Ledger);
+        true -> {ok, ToCheck}
+    end.
+
+-spec h3_to_region_(H3 :: h3:h3_index(), Ledger :: blockchain_ledger_v1:ledger()) ->
+    {ok, atom()} | {error, any()}.
+h3_to_region_(H3, Ledger) ->
+    case get_all_regions(Ledger) of
+        {ok, Regions} ->
+            region_(Regions, H3, Ledger);
+        E ->
+            E
+    end.
+
+-spec h3_in_region_(
+    H3 :: h3:h3_index(),
+    RegionVar :: atom(),
+    Ledger :: blockchain_ledger_v1:ledger()
+) -> boolean() | {error, any()}.
+h3_in_region_(H3, RegionVar, Ledger) ->
     case blockchain:config(RegionVar, Ledger) of
         {ok, Bin} ->
             try h3:contains(H3, Bin) of
@@ -61,22 +116,4 @@ h3_in_region(H3, RegionVar, Ledger) ->
             end;
         _ ->
             {error, {region_var_not_set, RegionVar}}
-    end.
-
-%%--------------------------------------------------------------------
-%% helpers
-%%--------------------------------------------------------------------
--spec region_(
-    Regions :: regions(),
-    H3 :: h3:h3_index(),
-    Ledger :: blockchain_ledger_v1:ledger()
-) ->
-    {ok, atom()} | {error, any()}.
-region_([], H3, _Ledger) ->
-    {error, {unknown_region, H3}};
-region_([ToCheck | Remaining], H3, Ledger) ->
-    case h3_in_region(H3, ToCheck, Ledger) of
-        {error, _} = Error -> Error;
-        false -> region_(Remaining, H3, Ledger);
-        true -> {ok, ToCheck}
     end.
