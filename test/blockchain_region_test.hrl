3d2f31167482fb58fb191740316f7b66d011c05e
Rahul Garg <rg@vihu.dev>
2021-09-21 12:08:11 -0700

    Cleanup tests and update region parameters

--- test/blockchain_region_test.hrl
+++ test/blockchain_region_test.hrl
@@ -81,34 +81,6 @@
 
 -define(REGION_PARAMS_EU868, [
     [
-        {<<"channel_frequency">>, 868100000},
-        {<<"bandwidth">>, 125000},
-        {<<"max_eirp">>, 161},
-        {<<"spreading">>,
-         [{65, 'SF12'}, {129, 'SF9'}, {238, 'SF8'}]}
-    ],
-    [
-        {<<"channel_frequency">>, 868300000},
-        {<<"bandwidth">>, 125000},
-        {<<"max_eirp">>, 161},
-        {<<"spreading">>,
-         [{65, 'SF12'}, {129, 'SF9'}, {238, 'SF8'}]}
-    ],
-    [
-        {<<"channel_frequency">>, 868500000},
-        {<<"bandwidth">>, 125000},
-        {<<"max_eirp">>, 161},
-        {<<"spreading">>,
-         [{65, 'SF12'}, {129, 'SF9'}, {238, 'SF8'}]}
-    ],
-    [
-        {<<"channel_frequency">>, 868500000},
-        {<<"bandwidth">>, 125000},
-        {<<"max_eirp">>, 161},
-        {<<"spreading">>,
-         [{65, 'SF12'}, {129, 'SF9'}, {238, 'SF8'}]}
-    ],
-    [
         {<<"channel_frequency">>, 867100000},
         {<<"bandwidth">>, 125000},
         {<<"max_eirp">>, 161},
@@ -142,6 +114,110 @@
         {<<"max_eirp">>, 161},
         {<<"spreading">>,
          [{65, 'SF12'}, {129, 'SF9'}, {238, 'SF8'}]}
+    ],
+    [
+        {<<"channel_frequency">>, 868100000},
+        {<<"bandwidth">>, 125000},
+        {<<"max_eirp">>, 161},
+        {<<"spreading">>,
+         [{65, 'SF12'}, {129, 'SF9'}, {238, 'SF8'}]}
+    ],
+    [
+        {<<"channel_frequency">>, 868300000},
+        {<<"bandwidth">>, 125000},
+        {<<"max_eirp">>, 161},
+        {<<"spreading">>,
+         [{65, 'SF12'}, {129, 'SF9'}, {238, 'SF8'}]}
+    ],
+    [
+        {<<"channel_frequency">>, 868500000},
+        {<<"bandwidth">>, 125000},
+        {<<"max_eirp">>, 161},
+        {<<"spreading">>,
+         [{65, 'SF12'}, {129, 'SF9'}, {238, 'SF8'}]}
+    ]
+]).
+
+-define(REGION_PARAMS_EU433, [
+    [
+        {<<"channel_frequency">>, 43317500},
+        {<<"bandwidth">>, 125000},
+        {<<"max_eirp">>, 121},
+        {<<"spreading">>,
+         [{25, 'SF12'}, {25, 'SF11'}, {25, 'SF10'}, {67, 'SF9'}, {139, 'SF8'}, {256, 'SF7'}]}
+    ],
+    [
+        {<<"channel_frequency">>, 43337500},
+        {<<"bandwidth">>, 125000},
+        {<<"max_eirp">>, 121},
+        {<<"spreading">>,
+         [{25, 'SF12'}, {25, 'SF11'}, {25, 'SF10'}, {67, 'SF9'}, {139, 'SF8'}, {256, 'SF7'}]}
+    ],
+    [
+        {<<"channel_frequency">>, 43357500},
+        {<<"bandwidth">>, 125000},
+        {<<"max_eirp">>, 121},
+        {<<"spreading">>,
+         [{25, 'SF12'}, {25, 'SF11'}, {25, 'SF10'}, {67, 'SF9'}, {139, 'SF8'}, {256, 'SF7'}]}
+    ]
+]).
+
+-define(REGION_PARAMS_CN470, [
+    [
+        {<<"channel_frequency">>, 486300000},
+        {<<"bandwidth">>, 125000},
+        {<<"max_eirp">>, 191},
+        {<<"spreading">>,
+         [{25, 'SF12'}, {25, 'SF11'}, {25, 'SF10'}, {67, 'SF9'}, {139, 'SF8'}, {256, 'SF7'}]}
+    ],
+    [
+        {<<"channel_frequency">>, 486500000},
+        {<<"bandwidth">>, 125000},
+        {<<"max_eirp">>, 191},
+        {<<"spreading">>,
+         [{25, 'SF12'}, {25, 'SF11'}, {25, 'SF10'}, {67, 'SF9'}, {139, 'SF8'}, {256, 'SF7'}]}
+    ],
+    [
+        {<<"channel_frequency">>, 486700000},
+        {<<"bandwidth">>, 125000},
+        {<<"max_eirp">>, 191},
+        {<<"spreading">>,
+         [{25, 'SF12'}, {25, 'SF11'}, {25, 'SF10'}, {67, 'SF9'}, {139, 'SF8'}, {256, 'SF7'}]}
+    ],
+    [
+        {<<"channel_frequency">>, 486900000},
+        {<<"bandwidth">>, 125000},
+        {<<"max_eirp">>, 191},
+        {<<"spreading">>,
+         [{25, 'SF12'}, {25, 'SF11'}, {25, 'SF10'}, {67, 'SF9'}, {139, 'SF8'}, {256, 'SF7'}]}
+    ],
+    [
+        {<<"channel_frequency">>, 487100000},
+        {<<"bandwidth">>, 125000},
+        {<<"max_eirp">>, 191},
+        {<<"spreading">>,
+         [{25, 'SF12'}, {25, 'SF11'}, {25, 'SF10'}, {67, 'SF9'}, {139, 'SF8'}, {256, 'SF7'}]}
+    ],
+    [
+        {<<"channel_frequency">>, 487300000},
+        {<<"bandwidth">>, 125000},
+        {<<"max_eirp">>, 191},
+        {<<"spreading">>,
+         [{25, 'SF12'}, {25, 'SF11'}, {25, 'SF10'}, {67, 'SF9'}, {139, 'SF8'}, {256, 'SF7'}]}
+    ],
+    [
+        {<<"channel_frequency">>, 487500000},
+        {<<"bandwidth">>, 125000},
+        {<<"max_eirp">>, 191},
+        {<<"spreading">>,
+         [{25, 'SF12'}, {25, 'SF11'}, {25, 'SF10'}, {67, 'SF9'}, {139, 'SF8'}, {256, 'SF7'}]}
+    ],
+    [
+        {<<"channel_frequency">>, 487700000},
+        {<<"bandwidth">>, 125000},
+        {<<"max_eirp">>, 191},
+        {<<"spreading">>,
+         [{25, 'SF12'}, {25, 'SF11'}, {25, 'SF10'}, {67, 'SF9'}, {139, 'SF8'}, {256, 'SF7'}]}
     ]
 ]).
 
@@ -220,65 +296,6 @@
          [{25, 'SF12'}, {25, 'SF11'}, {25, 'SF10'}, {67, 'SF9'}, {139, 'SF8'}, {256, 'SF7'}]}
     ],
     [
-        {<<"channel_frequency">>, 922200000},
-        {<<"bandwidth">>, 125000},
-        {<<"max_eirp">>, 160},
-        {<<"spreading">>,
-         [{25, 'SF12'}, {25, 'SF11'}, {25, 'SF10'}, {67, 'SF9'}, {139, 'SF8'}, {256, 'SF7'}]}
-    ],
-    [
-        {<<"channel_frequency">>, 922400000},
-        {<<"bandwidth">>, 125000},
-        {<<"max_eirp">>, 160},
-        {<<"spreading">>,
-         [{25, 'SF12'}, {25, 'SF11'}, {25, 'SF10'}, {67, 'SF9'}, {139, 'SF8'}, {256, 'SF7'}]}
-    ],
-    [
-        {<<"channel_frequency">>, 922600000},
-        {<<"bandwidth">>, 125000},
-        {<<"max_eirp">>, 160},
-        {<<"spreading">>,
-         [{25, 'SF12'}, {25, 'SF11'}, {25, 'SF10'}, {67, 'SF9'}, {139, 'SF8'}, {256, 'SF7'}]}
-    ],
-    [
-        {<<"channel_frequency">>, 922800000},
-        {<<"bandwidth">>, 125000},
-        {<<"max_eirp">>, 160},
-        {<<"spreading">>,
-         [{25, 'SF12'}, {25, 'SF11'}, {25, 'SF10'}, {67, 'SF9'}, {139, 'SF8'}, {256, 'SF7'}]}
-    ],
-    [
-        {<<"channel_frequency">>, 923000000},
-        {<<"bandwidth">>, 125000},
-        {<<"max_eirp">>, 160},
-        {<<"spreading">>,
-         [{25, 'SF12'}, {25, 'SF11'}, {25, 'SF10'}, {67, 'SF9'}, {139, 'SF8'}, {256, 'SF7'}]}
-    ],
-    [
-        {<<"channel_frequency">>, 922000000},
-        {<<"bandwidth">>, 125000},
-        {<<"max_eirp">>, 160},
-        {<<"spreading">>,
-         [{25, 'SF12'}, {25, 'SF11'}, {25, 'SF10'}, {67, 'SF9'}, {139, 'SF8'}, {256, 'SF7'}]}
-    ]
-]).
-
--define(REGION_PARAMS_AS923_2, [
-    [
-        {<<"channel_frequency">>, 923200000},
-        {<<"bandwidth">>, 125000},
-        {<<"max_eirp">>, 160},
-        {<<"spreading">>,
-         [{25, 'SF12'}, {25, 'SF11'}, {25, 'SF10'}, {67, 'SF9'}, {139, 'SF8'}, {256, 'SF7'}]}
-    ],
-    [
-        {<<"channel_frequency">>, 923400000},
-        {<<"bandwidth">>, 125000},
-        {<<"max_eirp">>, 160},
-        {<<"spreading">>,
-         [{25, 'SF12'}, {25, 'SF11'}, {25, 'SF10'}, {67, 'SF9'}, {139, 'SF8'}, {256, 'SF7'}]}
-    ],
-    [
         {<<"channel_frequency">>, 923600000},
         {<<"bandwidth">>, 125000},
         {<<"max_eirp">>, 160},
@@ -322,6 +339,65 @@
     ]
 ]).
 
+-define(REGION_PARAMS_AS923_2, [
+    [
+        {<<"channel_frequency">>, 921400000},
+        {<<"bandwidth">>, 125000},
+        {<<"max_eirp">>, 160},
+        {<<"spreading">>,
+         [{25, 'SF12'}, {25, 'SF11'}, {25, 'SF10'}, {67, 'SF9'}, {139, 'SF8'}, {256, 'SF7'}]}
+    ],
+    [
+        {<<"channel_frequency">>, 921600000},
+        {<<"bandwidth">>, 125000},
+        {<<"max_eirp">>, 160},
+        {<<"spreading">>,
+         [{25, 'SF12'}, {25, 'SF11'}, {25, 'SF10'}, {67, 'SF9'}, {139, 'SF8'}, {256, 'SF7'}]}
+    ],
+    [
+        {<<"channel_frequency">>, 921800000},
+        {<<"bandwidth">>, 125000},
+        {<<"max_eirp">>, 160},
+        {<<"spreading">>,
+         [{25, 'SF12'}, {25, 'SF11'}, {25, 'SF10'}, {67, 'SF9'}, {139, 'SF8'}, {256, 'SF7'}]}
+    ],
+    [
+        {<<"channel_frequency">>, 922000000},
+        {<<"bandwidth">>, 125000},
+        {<<"max_eirp">>, 160},
+        {<<"spreading">>,
+         [{25, 'SF12'}, {25, 'SF11'}, {25, 'SF10'}, {67, 'SF9'}, {139, 'SF8'}, {256, 'SF7'}]}
+    ],
+    [
+        {<<"channel_frequency">>, 922200000},
+        {<<"bandwidth">>, 125000},
+        {<<"max_eirp">>, 160},
+        {<<"spreading">>,
+         [{25, 'SF12'}, {25, 'SF11'}, {25, 'SF10'}, {67, 'SF9'}, {139, 'SF8'}, {256, 'SF7'}]}
+    ],
+    [
+        {<<"channel_frequency">>, 922400000},
+        {<<"bandwidth">>, 125000},
+        {<<"max_eirp">>, 160},
+        {<<"spreading">>,
+         [{25, 'SF12'}, {25, 'SF11'}, {25, 'SF10'}, {67, 'SF9'}, {139, 'SF8'}, {256, 'SF7'}]}
+    ],
+    [
+        {<<"channel_frequency">>, 922600000},
+        {<<"bandwidth">>, 125000},
+        {<<"max_eirp">>, 160},
+        {<<"spreading">>,
+         [{25, 'SF12'}, {25, 'SF11'}, {25, 'SF10'}, {67, 'SF9'}, {139, 'SF8'}, {256, 'SF7'}]}
+    ],
+    [
+        {<<"channel_frequency">>, 922800000},
+        {<<"bandwidth">>, 125000},
+        {<<"max_eirp">>, 160},
+        {<<"spreading">>,
+         [{25, 'SF12'}, {25, 'SF11'}, {25, 'SF10'}, {67, 'SF9'}, {139, 'SF8'}, {256, 'SF7'}]}
+    ]
+]).
+
 -define(REGION_PARAMS_AS923_3, [
     [
         {<<"channel_frequency">>, 916600000},
@@ -336,8 +412,49 @@
         {<<"max_eirp">>, 160},
         {<<"spreading">>,
          [{25, 'SF12'}, {25, 'SF11'}, {25, 'SF10'}, {67, 'SF9'}, {139, 'SF8'}, {256, 'SF7'}]}
+    ],
+    [
+        {<<"channel_frequency">>, 917000000},
+        {<<"bandwidth">>, 125000},
+        {<<"max_eirp">>, 160},
+        {<<"spreading">>,
+         [{25, 'SF12'}, {25, 'SF11'}, {25, 'SF10'}, {67, 'SF9'}, {139, 'SF8'}, {256, 'SF7'}]}
+    ],
+    [
+        {<<"channel_frequency">>, 917200000},
+        {<<"bandwidth">>, 125000},
+        {<<"max_eirp">>, 160},
+        {<<"spreading">>,
+         [{25, 'SF12'}, {25, 'SF11'}, {25, 'SF10'}, {67, 'SF9'}, {139, 'SF8'}, {256, 'SF7'}]}
+    ],
+    [
+        {<<"channel_frequency">>, 917400000},
+        {<<"bandwidth">>, 125000},
+        {<<"max_eirp">>, 160},
+        {<<"spreading">>,
+         [{25, 'SF12'}, {25, 'SF11'}, {25, 'SF10'}, {67, 'SF9'}, {139, 'SF8'}, {256, 'SF7'}]}
+    ],
+    [
+        {<<"channel_frequency">>, 917600000},
+        {<<"bandwidth">>, 125000},
+        {<<"max_eirp">>, 160},
+        {<<"spreading">>,
+         [{25, 'SF12'}, {25, 'SF11'}, {25, 'SF10'}, {67, 'SF9'}, {139, 'SF8'}, {256, 'SF7'}]}
+    ],
+    [
+        {<<"channel_frequency">>, 917800000},
+        {<<"bandwidth">>, 125000},
+        {<<"max_eirp">>, 160},
+        {<<"spreading">>,
+         [{25, 'SF12'}, {25, 'SF11'}, {25, 'SF10'}, {67, 'SF9'}, {139, 'SF8'}, {256, 'SF7'}]}
+    ],
+    [
+        {<<"channel_frequency">>, 918000000},
+        {<<"bandwidth">>, 125000},
+        {<<"max_eirp">>, 160},
+        {<<"spreading">>,
+         [{25, 'SF12'}, {25, 'SF11'}, {25, 'SF10'}, {67, 'SF9'}, {139, 'SF8'}, {256, 'SF7'}]}
     ]
-    %% This is incomplete...
 ]).
 
 -define(REGION_PARAMS_AS923_4, [
@@ -354,105 +471,46 @@
         {<<"max_eirp">>, 160},
         {<<"spreading">>,
          [{25, 'SF12'}, {25, 'SF11'}, {25, 'SF10'}, {67, 'SF9'}, {139, 'SF8'}, {256, 'SF7'}]}
-    ]
-    %% This is incomplete...
-]).
-
--define(REGION_PARAMS_RU864, [
+    ],
     [
-        {<<"channel_frequency">>, 868900000},
+        {<<"channel_frequency">>, 917700000},
         {<<"bandwidth">>, 125000},
         {<<"max_eirp">>, 160},
         {<<"spreading">>,
          [{25, 'SF12'}, {25, 'SF11'}, {25, 'SF10'}, {67, 'SF9'}, {139, 'SF8'}, {256, 'SF7'}]}
     ],
     [
-        {<<"channel_frequency">>, 869100000},
+        {<<"channel_frequency">>, 917900000},
         {<<"bandwidth">>, 125000},
         {<<"max_eirp">>, 160},
         {<<"spreading">>,
          [{25, 'SF12'}, {25, 'SF11'}, {25, 'SF10'}, {67, 'SF9'}, {139, 'SF8'}, {256, 'SF7'}]}
-    ]
-]).
-
--define(REGION_PARAMS_CN470, [
+    ],
     [
-        {<<"channel_frequency">>, 486300000},
+        {<<"channel_frequency">>, 918100000},
         {<<"bandwidth">>, 125000},
-        {<<"max_eirp">>, 191},
+        {<<"max_eirp">>, 160},
         {<<"spreading">>,
          [{25, 'SF12'}, {25, 'SF11'}, {25, 'SF10'}, {67, 'SF9'}, {139, 'SF8'}, {256, 'SF7'}]}
     ],
     [
-        {<<"channel_frequency">>, 486500000},
+        {<<"channel_frequency">>, 918300000},
         {<<"bandwidth">>, 125000},
-        {<<"max_eirp">>, 191},
+        {<<"max_eirp">>, 160},
         {<<"spreading">>,
          [{25, 'SF12'}, {25, 'SF11'}, {25, 'SF10'}, {67, 'SF9'}, {139, 'SF8'}, {256, 'SF7'}]}
     ],
     [
-        {<<"channel_frequency">>, 486700000},
+        {<<"channel_frequency">>, 918500000},
         {<<"bandwidth">>, 125000},
-        {<<"max_eirp">>, 191},
+        {<<"max_eirp">>, 160},
         {<<"spreading">>,
          [{25, 'SF12'}, {25, 'SF11'}, {25, 'SF10'}, {67, 'SF9'}, {139, 'SF8'}, {256, 'SF7'}]}
     ],
     [
-        {<<"channel_frequency">>, 486900000},
+        {<<"channel_frequency">>, 918700000},
         {<<"bandwidth">>, 125000},
-        {<<"max_eirp">>, 191},
-        {<<"spreading">>,
-         [{25, 'SF12'}, {25, 'SF11'}, {25, 'SF10'}, {67, 'SF9'}, {139, 'SF8'}, {256, 'SF7'}]}
-    ],
-    [
-        {<<"channel_frequency">>, 487100000},
-        {<<"bandwidth">>, 125000},
-        {<<"max_eirp">>, 191},
-        {<<"spreading">>,
-         [{25, 'SF12'}, {25, 'SF11'}, {25, 'SF10'}, {67, 'SF9'}, {139, 'SF8'}, {256, 'SF7'}]}
-    ],
-    [
-        {<<"channel_frequency">>, 487300000},
-        {<<"bandwidth">>, 125000},
-        {<<"max_eirp">>, 191},
-        {<<"spreading">>,
-         [{25, 'SF12'}, {25, 'SF11'}, {25, 'SF10'}, {67, 'SF9'}, {139, 'SF8'}, {256, 'SF7'}]}
-    ],
-    [
-        {<<"channel_frequency">>, 487500000},
-        {<<"bandwidth">>, 125000},
-        {<<"max_eirp">>, 191},
-        {<<"spreading">>,
-         [{25, 'SF12'}, {25, 'SF11'}, {25, 'SF10'}, {67, 'SF9'}, {139, 'SF8'}, {256, 'SF7'}]}
-    ],
-    [
-        {<<"channel_frequency">>, 487700000},
-        {<<"bandwidth">>, 125000},
-        {<<"max_eirp">>, 191},
-        {<<"spreading">>,
-         [{25, 'SF12'}, {25, 'SF11'}, {25, 'SF10'}, {67, 'SF9'}, {139, 'SF8'}, {256, 'SF7'}]}
-    ]
-]).
-
--define(REGION_PARAMS_IN865, [
-    [
-        {<<"channel_frequency">>, 865062500},
-        {<<"bandwidth">>, 125000},
-        {<<"max_eirp">>, 300},
-        {<<"spreading">>,
-         [{25, 'SF12'}, {25, 'SF11'}, {25, 'SF10'}, {67, 'SF9'}, {139, 'SF8'}, {256, 'SF7'}]}
-    ],
-    [
-        {<<"channel_frequency">>, 865985000},
-        {<<"bandwidth">>, 125000},
-        {<<"max_eirp">>, 300},
-        {<<"spreading">>,
-         [{25, 'SF12'}, {25, 'SF11'}, {25, 'SF10'}, {67, 'SF9'}, {139, 'SF8'}, {256, 'SF7'}]}
-    ],
-    [
-        {<<"channel_frequency">>, 865402500},
-        {<<"bandwidth">>, 125000},
-        {<<"max_eirp">>, 300},
+        {<<"max_eirp">>, 160},
         {<<"spreading">>,
          [{25, 'SF12'}, {25, 'SF11'}, {25, 'SF10'}, {67, 'SF9'}, {139, 'SF8'}, {256, 'SF7'}]}
     ]
@@ -510,25 +568,78 @@
     ]
 ]).
 
--define(REGION_PARAMS_EU433, [
+-define(REGION_PARAMS_IN865, [
     [
-        {<<"channel_frequency">>, 43317500},
+        {<<"channel_frequency">>, 865062500},
         {<<"bandwidth">>, 125000},
-        {<<"max_eirp">>, 121},
+        {<<"max_eirp">>, 300},
         {<<"spreading">>,
          [{25, 'SF12'}, {25, 'SF11'}, {25, 'SF10'}, {67, 'SF9'}, {139, 'SF8'}, {256, 'SF7'}]}
     ],
     [
-        {<<"channel_frequency">>, 43337500},
+        {<<"channel_frequency">>, 865402500},
         {<<"bandwidth">>, 125000},
-        {<<"max_eirp">>, 121},
+        {<<"max_eirp">>, 300},
         {<<"spreading">>,
          [{25, 'SF12'}, {25, 'SF11'}, {25, 'SF10'}, {67, 'SF9'}, {139, 'SF8'}, {256, 'SF7'}]}
     ],
     [
-        {<<"channel_frequency">>, 43357500},
+        {<<"channel_frequency">>, 865985000},
         {<<"bandwidth">>, 125000},
-        {<<"max_eirp">>, 121},
+        {<<"max_eirp">>, 300},
+        {<<"spreading">>,
+         [{25, 'SF12'}, {25, 'SF11'}, {25, 'SF10'}, {67, 'SF9'}, {139, 'SF8'}, {256, 'SF7'}]}
+    ]
+]).
+
+
+-define(REGION_PARAMS_RU864, [
+    [
+        {<<"channel_frequency">>, 864100000},
+        {<<"bandwidth">>, 125000},
+        {<<"max_eirp">>, 160},
+        {<<"spreading">>,
+         [{25, 'SF12'}, {25, 'SF11'}, {25, 'SF10'}, {67, 'SF9'}, {139, 'SF8'}, {256, 'SF7'}]}
+    ],
+    [
+        {<<"channel_frequency">>, 864300000},
+        {<<"bandwidth">>, 125000},
+        {<<"max_eirp">>, 160},
+        {<<"spreading">>,
+         [{25, 'SF12'}, {25, 'SF11'}, {25, 'SF10'}, {67, 'SF9'}, {139, 'SF8'}, {256, 'SF7'}]}
+    ],
+    [
+        {<<"channel_frequency">>, 864500000},
+        {<<"bandwidth">>, 125000},
+        {<<"max_eirp">>, 160},
+        {<<"spreading">>,
+         [{25, 'SF12'}, {25, 'SF11'}, {25, 'SF10'}, {67, 'SF9'}, {139, 'SF8'}, {256, 'SF7'}]}
+    ],
+    [
+        {<<"channel_frequency">>, 864700000},
+        {<<"bandwidth">>, 125000},
+        {<<"max_eirp">>, 160},
+        {<<"spreading">>,
+         [{25, 'SF12'}, {25, 'SF11'}, {25, 'SF10'}, {67, 'SF9'}, {139, 'SF8'}, {256, 'SF7'}]}
+    ],
+    [
+        {<<"channel_frequency">>, 864900000},
+        {<<"bandwidth">>, 125000},
+        {<<"max_eirp">>, 160},
+        {<<"spreading">>,
+         [{25, 'SF12'}, {25, 'SF11'}, {25, 'SF10'}, {67, 'SF9'}, {139, 'SF8'}, {256, 'SF7'}]}
+    ],
+    [
+        {<<"channel_frequency">>, 868900000},
+        {<<"bandwidth">>, 125000},
+        {<<"max_eirp">>, 160},
+        {<<"spreading">>,
+         [{25, 'SF12'}, {25, 'SF11'}, {25, 'SF10'}, {67, 'SF9'}, {139, 'SF8'}, {256, 'SF7'}]}
+    ],
+    [
+        {<<"channel_frequency">>, 869100000},
+        {<<"bandwidth">>, 125000},
+        {<<"max_eirp">>, 160},
         {<<"spreading">>,
          [{25, 'SF12'}, {25, 'SF11'}, {25, 'SF10'}, {67, 'SF9'}, {139, 'SF8'}, {256, 'SF7'}]}
     ]
