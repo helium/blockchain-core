d5fecc9f54f9fc8e994f3bcbabdfd9afbf60b1af
Siraaj Khandkar <siraaj@khandkar.net>
2021-10-04 10:26:51 -0400

    Add a more-flexible version of legacy chain-init

--- test/blockchain_implicit_burn_SUITE.erl
+++ test/blockchain_implicit_burn_SUITE.erl
@@ -32,7 +32,14 @@ init_per_testcase(TestCase, Config) ->
     ExtraVars = extra_vars(TestCase),
 
     {ok, GenesisMembers, _GenesisBlock, ConsensusMembers, Keys} =
-        test_utils:init_chain(Balance, {PrivKey, PubKey}, true, ExtraVars),
+        test_utils:init_chain_with_opts(
+            #{
+                balance => Balance,
+                keys => {PrivKey, PubKey},
+                in_consensus => true,
+                extra_vars => ExtraVars
+            }
+        ),
 
     Chain = blockchain_worker:blockchain(),
     Swarm = blockchain_swarm:swarm(),
