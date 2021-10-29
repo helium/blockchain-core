ae6df04fb15b31c9c6aeeead6f6362f6ec11b3fe
Siraaj Khandkar <siraaj@khandkar.net>
2021-10-29 10:35:02 -0400

    Support empty result via explicit conversion

--- src/result.erl
+++ src/result.erl
@@ -1,18 +1,31 @@
 -module(result).
 
 -export_type([
+    empty/1,
     t/2
 ]).
 
 -export([
+    of_empty/2,
+    to_empty/1,
     of_bool/3,
     to_bool/1,
     pipe/2
 ]).
 
+-type empty(ErrorReason) ::
+    ok | {error, ErrorReason}.
+
 -type t(Ok, Error) ::
     {ok, Ok} | {error, Error}. 
-    %% TODO Reconsider adding 'ok' case. How should pipe handle it?
+
+-spec of_empty(empty(ErrorReason), OkVal) -> t(OkVal, ErrorReason).
+of_empty(ok, OkVal) -> {ok, OkVal};
+of_empty({error, _}=Error, _) -> Error.
+
+-spec to_empty(t(_, ErrorReason)) -> empty(ErrorReason).
+to_empty({ok, _}) -> ok;
+to_empty({error, _}=E) -> E.
 
 -spec of_bool(boolean(), Ok, Error) -> t(Ok, Error).
 of_bool(true, Ok, _) -> {ok, Ok};
