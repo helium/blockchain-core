1ca572c841577fc42f8c16fcb6f6752415ff8e79
Siraaj Khandkar <siraaj@khandkar.net>
2021-11-06 00:28:11 -0400

    Implement is_well_formed in blockchain_txn_unstake_validator_v1

--- src/transactions/v1/blockchain_txn_unstake_validator_v1.erl
+++ src/transactions/v1/blockchain_txn_unstake_validator_v1.erl
@@ -6,12 +6,14 @@
 -module(blockchain_txn_unstake_validator_v1).
 
 -behavior(blockchain_txn).
-
 -behavior(blockchain_json).
+
 -include("blockchain_json.hrl").
 -include("blockchain_utils.hrl").
 -include("blockchain_txn_fees.hrl").
 -include("blockchain_vars.hrl").
+-include("blockchain_records_meta.hrl").
+
 -include_lib("helium_proto/include/blockchain_txn_unstake_validator_v1_pb.hrl").
 
 -export([
@@ -189,8 +191,18 @@ is_valid(Txn, Chain) ->
     end.
 
 -spec is_well_formed(txn_unstake_validator()) -> ok | {error, _}.
-is_well_formed(_Txn) ->
-    error(not_implemented).
+is_well_formed(#blockchain_txn_unstake_validator_v1_pb{}=T) ->
+    blockchain_contract:check(
+        record_to_kvl(blockchain_txn_unstake_validator_v1_pb, T),
+        {kvl, [
+            {address                 , {address, libp2p}},
+            {owner                   , {address, libp2p}},
+            {owner_signature         , {binary, any}},
+            {fee                     , {integer, {min, 0}}},
+            {stake_amount            , {integer, {min, 0}}},
+            {stake_release_height    , {integer, {min, 0}}}
+        ]}
+    ).
 
 -spec is_absorbable(txn_unstake_validator(), blockchain:blockchain()) ->
     boolean().
@@ -239,6 +251,9 @@ to_json(Txn, _Opts) ->
       stake_release_height => stake_release_height(Txn)
      }.
 
+-spec record_to_kvl(atom(), tuple()) -> [{atom(), term()}].
+?DEFINE_RECORD_TO_KVL(blockchain_txn_unstake_validator_v1_pb).
+
 %% ------------------------------------------------------------------
 %% EUNIT Tests
 %% ------------------------------------------------------------------
@@ -250,7 +265,20 @@ to_json_test() ->
     ?assertEqual(lists:sort(maps:keys(Json)),
                  lists:sort([type, hash] ++ record_info(fields, blockchain_txn_unstake_validator_v1_pb))).
 
-validation_test() ->
-    error('TODO-validation_test').
+is_well_formed_test_() ->
+    Addr =
+        begin
+            #{public := P, secret := _} = libp2p_crypto:generate_keys(ecc_compact),
+            libp2p_crypto:pubkey_to_bin(P)
+        end,
+    T =
+        #blockchain_txn_unstake_validator_v1_pb{
+            address = Addr,
+            owner = Addr
+            %% For the rest, defaults should be sufficient.
+        },
+    [
+        ?_assertMatch(ok, is_well_formed(T))
+    ].
 
 -endif.
