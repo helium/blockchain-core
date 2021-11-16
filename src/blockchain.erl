ae6893e0275c4fc6b98ef4b91c9fa7a32a9846a2
Andrew Thompson <andrew@hijacked.us>
2021-11-16 05:11:55 -0800

    Remove two more unnecessary uses of head_block

--- src/blockchain.erl
+++ src/blockchain.erl
@@ -464,11 +464,7 @@ height(Blockchain) ->
         {error, _}=Error ->
             Error;
         {ok, Hash} ->
-            case get_block(Hash, Blockchain) of
-                {error, _Reason}=Error -> Error;
-                {ok, Block} ->
-                    {ok, blockchain_block:height(Block)}
-            end
+            get_block_height(Hash, Blockchain)
     end.
 
 %% like height/1 but takes accumulated 'assumed valid' blocks into account
@@ -917,12 +913,11 @@ can_add_block(Block, Blockchain) ->
         true ->
             {error, unknown_genesis_block};
         false ->
-            case blockchain:head_block(Blockchain) of
+            case blockchain:head_block_info(Blockchain) of
                 {error, Reason}=Error ->
                     lager:error("could not get head hash ~p", [Reason]),
                     Error;
-                {ok, HeadBlock} ->
-                    HeadHash = blockchain_block:hash_block(HeadBlock),
+                {ok, #block_info{hash=HeadHash, height=HeadHeight}} ->
                     Height = blockchain_block:height(Block),
                     {ok, ChainHeight} = blockchain:height(Blockchain),
                     %% compute the ledger at the height of the chain in case we're
@@ -931,7 +926,7 @@ can_add_block(Block, Blockchain) ->
                     {ok, Ledger} = blockchain:ledger_at(ChainHeight, Blockchain),
                     case
                         blockchain_block:prev_hash(Block) =:= HeadHash andalso
-                         Height =:= blockchain_block:height(HeadBlock) + 1
+                         Height =:= HeadHeight + 1
                     of
                         false when HeadHash =:= Hash ->
                             lager:debug("Already have this block"),
@@ -950,12 +945,12 @@ can_add_block(Block, Blockchain) ->
                                                 true -> plausible;
                                                 false ->
                                                     lager:warning("higher block doesn't fit with our chain, block_height: ~p, head_block_height: ~p", [blockchain_block:height(Block),
-                                                                                                                                                blockchain_block:height(HeadBlock)]),
+                                                                                                                                                HeadHeight]),
                                                     {error, disjoint_chain}
                                             end;
                                         false ->
                                             lager:warning("lower block doesn't fit with our chain, block_height: ~p, head_block_height: ~p", [blockchain_block:height(Block),
-                                                                                                                                              blockchain_block:height(HeadBlock)]),
+                                                                                                                                              HeadHeight]),
                                             %% if the block height is lower we probably don't care about it
                                             {error, disjoint_chain}
                                     end
