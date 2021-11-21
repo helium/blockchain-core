fbbda57d9adc17b4606d7a1afb01764b7e7be1d7
macpie <pierre@helium.com>
2021-11-20 19:05:06 -0800

    Sign at the edge to avoid work in SC worker

--- src/state_channel/blockchain_state_channels_worker.erl
+++ src/state_channel/blockchain_state_channels_worker.erl
@@ -252,18 +252,18 @@ offer(
                     {noreply, State0};
                 {ok, PurchaseSC} ->
                     lager:debug("[~p] purchasing offer from ~p", [blockchain_state_channel_v1:id(PurchaseSC), HotspotName]),
-                    SignedPurchaseSC = blockchain_state_channel_v1:sign(PurchaseSC, OwnerSigFun),
                     PacketHash = blockchain_state_channel_offer_v1:packet_hash(Offer),
                     Region = blockchain_state_channel_offer_v1:region(Offer),
                     ok = blockchain_state_channel_common:send_purchase(
                         HandlerPid,
-                        SignedPurchaseSC,
+                        PurchaseSC,
                         HotspotID,
                         PacketHash,
-                        Region
+                        Region,
+                        OwnerSigFun
                     ),
-                    ok = blockchain_state_channel_v1:save(DB, SignedPurchaseSC, Skewed),
-                    {noreply, State0#state{state_channel=SignedPurchaseSC}}
+                    ok = blockchain_state_channel_v1:save(DB, PurchaseSC, Skewed),
+                    {noreply, State0#state{state_channel=PurchaseSC}}
             end
     end.
 
@@ -279,7 +279,6 @@ packet(
         state_channel=SC0,
         skewed=Skewed0,
         db=DB,
-        owner={_Owner, OwnerSigFun},
         dc_payload_size=DCPayloadSize,
         sc_version=SCVer,
         max_actors_allowed = MaxActorsAllowed,
@@ -312,9 +311,8 @@ packet(
                         ),
                     SC
             end,
-            SignedSC = blockchain_state_channel_v1:sign(SC2, OwnerSigFun),
-            ok = blockchain_state_channel_v1:save(DB, SignedSC, Skewed1),
-            State0#state{state_channel=SignedSC, skewed=Skewed1}
+            ok = blockchain_state_channel_v1:save(DB, SC2, Skewed1),
+            State0#state{state_channel=SC2, skewed=Skewed1}
     end.
 
 
