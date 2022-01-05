c8d8a3513dca3b27fc5b14fcd81cc49f427ccd81
macpie <pierre@helium.com>
2022-01-05 13:09:17 -0800

    Fix meck call

--- test/meck_test_util.erl
+++ test/meck_test_util.erl
@@ -7,4 +7,9 @@ forward_submit_txn(Pid) ->
         Pid ! {txn, T},
         ok
     end),
+    meck:expect(blockchain_worker, submit_txn, fun(T, F) ->
+        Pid ! {txn, T},
+        F(ok, T),
+        ok
+    end),
     ok.
