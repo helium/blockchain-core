19fdce4e579ed4f24156f63452d5132c8f488264
Andrew Thompson <andrew@hijacked.us>
2022-01-10 06:35:08 -0800

    Revert "Avoid PoC ops in follow mode"

--- src/transactions/v1/blockchain_txn_poc_receipts_v1.erl
+++ src/transactions/v1/blockchain_txn_poc_receipts_v1.erl
@@ -805,27 +805,21 @@ absorb(Txn, Chain) ->
     POCID = ?MODULE:poc_id(Txn),
 
     try
-        case blockchain:is_follow_mode() of
-            true ->
+        %% get these to make sure we're not replaying.
+        PoCs = case blockchain_ledger_v1:find_poc(LastOnionKeyHash, Ledger) of
+                   {ok, Ps} ->
+                       Ps;
+                   {error, not_found} ->
+                       lager:warning("potential replay: ~p not found", [LastOnionKeyHash]),
+                       throw(replay)
+               end,
+        case blockchain_ledger_poc_v2:find_valid(PoCs, Challenger, Secret) of
+            {ok, _PoC} ->
                 ok;
-            false ->
-                %% get these to make sure we're not replaying.
-                PoCs = case blockchain_ledger_v1:find_poc(LastOnionKeyHash, Ledger) of
-                           {ok, Ps} ->
-                               Ps;
-                           {error, not_found} ->
-                               lager:warning("potential replay: ~p not found", [LastOnionKeyHash]),
-                               throw(replay)
-                       end,
-                case blockchain_ledger_poc_v2:find_valid(PoCs, Challenger, Secret) of
-                    {ok, _PoC} ->
-                        ok;
-                    {error, not_found} ->
-                        lager:warning("potential replay: ~p ~p not in ~p", [Challenger, Secret, PoCs]),
-                        throw(replay)
-                end
+            {error, not_found} ->
+                lager:warning("potential replay: ~p ~p not in ~p", [Challenger, Secret, PoCs]),
+                throw(replay)
         end,
-
         {ok, LastChallenge} = blockchain_ledger_v1:find_gateway_last_challenge(Challenger, Ledger),
         PoCInterval = blockchain_utils:challenge_interval(Ledger),
         case LastChallenge + PoCInterval >= Height of
