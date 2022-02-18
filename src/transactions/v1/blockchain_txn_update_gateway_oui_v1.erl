98b8901c1c20d24dafd80b0e54507ee26868a67b
Siraaj Khandkar <siraaj@khandkar.net>
2022-02-18 16:51:55 -0500

    Implement blockchain_txn_update_gateway_oui_v1 contract check

--- src/transactions/v1/blockchain_txn_update_gateway_oui_v1.erl
+++ src/transactions/v1/blockchain_txn_update_gateway_oui_v1.erl
@@ -6,9 +6,10 @@
 -module(blockchain_txn_update_gateway_oui_v1).
 
 -behavior(blockchain_txn).
-
 -behavior(blockchain_json).
+
 -include("blockchain_json.hrl").
+-include("blockchain_records_meta.hrl").
 -include("blockchain_txn_fees.hrl").
 -include("blockchain_vars.hrl").
 -include_lib("helium_proto/include/blockchain_txn_update_gateway_oui_v1_pb.hrl").
@@ -44,11 +45,11 @@
 -include_lib("eunit/include/eunit.hrl").
 -endif.
 
--define(T, #blockchain_txn_update_gateway_oui_v1_pb).
+-define(T, blockchain_txn_update_gateway_oui_v1_pb).
 
 -type t() :: txn_update_gateway_oui().
 
--type txn_update_gateway_oui() :: ?T{}.
+-type txn_update_gateway_oui() :: #?T{}.
 
 -export_type([t/0, txn_update_gateway_oui/0]).
 
@@ -186,12 +187,22 @@ is_valid(Txn, Chain) ->
     end.
 
 -spec is_well_formed(t()) -> ok | {error, {contract_breach, any()}}.
-is_well_formed(?T{}) ->
-    ok.
+is_well_formed(#?T{}=T) ->
+    data_contract:check(
+        ?RECORD_TO_KVL(?T, T),
+        {kvl, [
+            {gateway                , {address, libp2p}},
+            {oui                    , {integer, {min, 0}}},
+            {nonce                  , {integer, {min, 1}}},
+            {fee                    , {integer, {min, 0}}},
+            {gateway_owner_signature, {binary, any}},
+            {oui_owner_signature    , {binary, any}}
+        ]}
+    ).
 
 -spec is_prompt(t(), blockchain_ledger_v1:ledger()) ->
     {ok, blockchain_txn:is_prompt()} | {error, any()}.
-is_prompt(?T{}, _) ->
+is_prompt(#?T{}, _) ->
     {ok, yes}.
 
 -spec absorb(txn_update_gateway_oui(), blockchain:blockchain()) -> ok | {error, atom()} | {error, {atom(), any()}}.
@@ -333,5 +344,20 @@ to_json_test() ->
     Json = to_json(Tx, []),
     ?assert(lists:all(fun(K) -> maps:is_key(K, Json) end,
                       [type, hash, gateway, oui, fee, nonce])).
+
+is_well_formed_test_() ->
+    Addr =
+        begin
+            #{public := P, secret := _} = libp2p_crypto:generate_keys(ecc_compact),
+            libp2p_crypto:pubkey_to_bin(P)
+        end,
+    T =
+        #?T{
+            gateway = Addr,
+            nonce   = 1
+        },
+    [
+        ?_assertMatch(ok, is_well_formed(T))
+    ].
 
 -endif.
