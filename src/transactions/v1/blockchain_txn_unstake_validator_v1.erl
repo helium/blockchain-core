d76ad41d46919741f16dbb9ac2be7f7a74bff839
Siraaj Khandkar <siraaj@khandkar.net>
2022-02-18 14:59:14 -0500

    Implement blockchain_txn_unstake_validator_v1 contract check

--- src/transactions/v1/blockchain_txn_unstake_validator_v1.erl
+++ src/transactions/v1/blockchain_txn_unstake_validator_v1.erl
@@ -6,10 +6,11 @@
 -module(blockchain_txn_unstake_validator_v1).
 
 -behavior(blockchain_txn).
-
 -behavior(blockchain_json).
+
 -include("blockchain_json.hrl").
 -include("blockchain_utils.hrl").
+-include("blockchain_records_meta.hrl").
 -include("blockchain_txn_fees.hrl").
 -include("blockchain_vars.hrl").
 -include_lib("helium_proto/include/blockchain_txn_unstake_validator_v1_pb.hrl").
@@ -39,11 +40,11 @@
 -export([is_valid_owner/1]).
 -endif.
 
--define(T, #blockchain_txn_unstake_validator_v1_pb).
+-define(T, blockchain_txn_unstake_validator_v1_pb).
 
 -type t() :: txn_unstake_validator().
 
--type txn_unstake_validator() :: ?T{}.
+-type txn_unstake_validator() :: #?T{}.
 
 -export_type([t/0, txn_unstake_validator/0]).
 
@@ -194,12 +195,22 @@ is_valid(Txn, Chain) ->
     end.
 
 -spec is_well_formed(t()) -> ok | {error, {contract_breach, any()}}.
-is_well_formed(?T{}) ->
-    ok.
+is_well_formed(#?T{}=T) ->
+    data_contract:check(
+        ?RECORD_TO_KVL(?T, T),
+        {kvl, [
+            {address                 , {address, libp2p}},
+            {owner                   , {address, libp2p}},
+            {owner_signature         , {binary, any}},
+            {fee                     , {integer, {min, 0}}},
+            {stake_amount            , {integer, {min, 0}}},
+            {stake_release_height    , {integer, {min, 0}}}
+        ]}
+    ).
 
 -spec is_prompt(t(), blockchain_ledger_v1:ledger()) ->
     {ok, blockchain_txn:is_prompt()} | {error, any()}.
-is_prompt(?T{}, _) ->
+is_prompt(#?T{}, _) ->
     {ok, yes}.
 
 -spec absorb(txn_unstake_validator(), blockchain:blockchain()) -> ok | {error, atom()} | {error, {atom(), any()}}.
@@ -255,5 +266,16 @@ to_json_test() ->
     ?assertEqual(lists:sort(maps:keys(Json)),
                  lists:sort([type, hash] ++ record_info(fields, blockchain_txn_unstake_validator_v1_pb))).
 
+
+is_well_formed_test_() ->
+    Addr =
+        begin
+            #{public := P, secret := _} = libp2p_crypto:generate_keys(ecc_compact),
+            libp2p_crypto:pubkey_to_bin(P)
+        end,
+    [
+        %% For other fields, defaults should be sufficient:
+        ?_assertMatch(ok, is_well_formed(#?T{address = Addr, owner = Addr}))
+    ].
 
 -endif.
