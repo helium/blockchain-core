8af5e7cb1aea1130e8314f303fb8b98cb1560686
Siraaj Khandkar <siraaj@khandkar.net>
2022-02-18 06:30:36 -0500

    Implement blockchain_txn_dc_coinbase_v1 contract check

--- src/transactions/v1/blockchain_txn_dc_coinbase_v1.erl
+++ src/transactions/v1/blockchain_txn_dc_coinbase_v1.erl
@@ -6,10 +6,10 @@
 -module(blockchain_txn_dc_coinbase_v1).
 
 -behavior(blockchain_txn).
-
 -behavior(blockchain_json).
--include("blockchain_json.hrl").
 
+-include("blockchain_json.hrl").
+-include("blockchain_records_meta.hrl").
 -include("blockchain_utils.hrl").
 -include_lib("helium_proto/include/blockchain_txn_dc_coinbase_v1_pb.hrl").
 
@@ -34,11 +34,11 @@
 -include_lib("eunit/include/eunit.hrl").
 -endif.
 
--define(T, #blockchain_txn_dc_coinbase_v1_pb).
+-define(T, blockchain_txn_dc_coinbase_v1_pb).
 
 -type t() :: txn_dc_coinbase().
 
--type txn_dc_coinbase() :: ?T{}.
+-type txn_dc_coinbase() :: #?T{}.
 
 -export_type([t/0, txn_dc_coinbase/0]).
 
@@ -119,12 +119,18 @@ is_valid(Txn, Chain) ->
     end.
 
 -spec is_well_formed(t()) -> ok | {error, {contract_breach, any()}}.
-is_well_formed(?T{}) ->
-    ok.
+is_well_formed(#?T{}=T) ->
+    data_contract:check(
+        ?RECORD_TO_KVL(blockchain_txn_dc_coinbase_v1_pb, T),
+        {kvl, [
+            {payee, {address, libp2p}},
+            {amount, {integer, {min, 0}}}
+        ]}
+    ).
 
 -spec is_prompt(t(), blockchain_ledger_v1:ledger()) ->
     {ok, blockchain_txn:is_prompt()} | {error, any()}.
-is_prompt(?T{}, _) ->
+is_prompt(#?T{}, _) ->
     {ok, yes}.
 
 %%--------------------------------------------------------------------
@@ -184,5 +190,25 @@ to_json_test() ->
     Json = to_json(Tx, []),
     ?assert(lists:all(fun(K) -> maps:is_key(K, Json) end,
                       [type, hash, payee, amount])).
+
+is_well_formed_test_() ->
+    Addr =
+        begin
+            #{public := PK, secret := _} =
+                libp2p_crypto:generate_keys(ecc_compact),
+            libp2p_crypto:pubkey_to_bin(PK)
+        end,
+    T =
+        #?T{
+            payee  = Addr,
+            amount = 0
+        },
+    [
+        ?_assertEqual(ok, is_well_formed(T)),
+        ?_assertMatch({error, _}, is_well_formed(T#?T{payee  = <<>>})),
+        ?_assertMatch({error, _}, is_well_formed(T#?T{payee  = undefined})),
+        ?_assertMatch({error, _}, is_well_formed(T#?T{amount = -1})),
+        ?_assertMatch({error, _}, is_well_formed(T#?T{amount = undefined}))
+    ].
 
 -endif.
