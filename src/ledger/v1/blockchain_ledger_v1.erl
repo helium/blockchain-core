f1e68da26d124146b3e2b7a52b89eca4ea24e45a
Andrew McKenzie <andrewb.mckenzie@gmail.com>
2022-05-20 17:27:41 +0100

    handle scenario when no block, such as when loading gen block during tests

--- src/ledger/v1/blockchain_ledger_v1.erl
+++ src/ledger/v1/blockchain_ledger_v1.erl
@@ -4914,32 +4914,37 @@ maybe_gc_h3dex(Ledger) ->
             %% from the current block (which are sorted by *challenger* and GC the
             %% hexes the *challengee* is in.
             {ok, Height} = current_height(Ledger),
-            %% If we can't get the block, we will just crash here
-            {ok, Block} = get_block(Height, Ledger),
-            {ok, #block_info_v2{hash = BlockHash}} = get_block_info(Height, Ledger),
-            RandState = blockchain_utils:rand_from_hash(BlockHash),
-            RequestFilter = fun(T) ->
-                                    blockchain_txn:type(T) == blockchain_txn_poc_receipts_v1
-                                    orelse blockchain_txn:type(T) == blockchain_txn_poc_receipts_v2
-                            end,
-            case blockchain_utils:find_txn(Block, RequestFilter) of
-                [] ->
-                    %% no receipts, don't do any GC
-                    ok;
-                Txns ->
-                    %% take the first `Width` receipts and GC the parent hexes of the challengees
-                    {_NewRand, Selected} = blockchain_utils:deterministic_subset(Width, RandState, Txns),
-                    lists:foreach(fun(T) ->
-                                          ReceiptType = blockchain_txn:type(T),
-                                          Path = ReceiptType:path(T),
-                                          Challengee = blockchain_poc_path_element_v1:challengee(hd(Path)),
-                                          case find_gateway_location(Challengee, Ledger) of
-                                              {ok, Location} ->
-                                                  gc_h3dex_hex(Location, Height, InactivityThreshold1, Ledger);
-                                              _ ->
-                                                  ok
-                                          end
-                                  end, Selected)
+            %% note: handling block not being found here as not doing so
+            %% results in tests failing during genesis block load
+            case get_block(Height, Ledger) of
+                {ok, Block} ->
+                    {ok, #block_info_v2{hash = BlockHash}} = get_block_info(Height, Ledger),
+                    RandState = blockchain_utils:rand_from_hash(BlockHash),
+                    RequestFilter = fun(T) ->
+                                            blockchain_txn:type(T) == blockchain_txn_poc_receipts_v1
+                                            orelse blockchain_txn:type(T) == blockchain_txn_poc_receipts_v2
+                                    end,
+                    case blockchain_utils:find_txn(Block, RequestFilter) of
+                        [] ->
+                            %% no receipts, don't do any GC
+                            ok;
+                        Txns ->
+                            %% take the first `Width` receipts and GC the parent hexes of the challengees
+                            {_NewRand, Selected} = blockchain_utils:deterministic_subset(Width, RandState, Txns),
+                            lists:foreach(fun(T) ->
+                                                  ReceiptType = blockchain_txn:type(T),
+                                                  Path = ReceiptType:path(T),
+                                                  Challengee = blockchain_poc_path_element_v1:challengee(hd(Path)),
+                                                  case find_gateway_location(Challengee, Ledger) of
+                                                      {ok, Location} ->
+                                                          gc_h3dex_hex(Location, Height, InactivityThreshold1, Ledger);
+                                                      _ ->
+                                                          ok
+                                                  end
+                                          end, Selected)
+                    end;
+                _ ->
+                    ok
             end;
         _ ->
             ok
