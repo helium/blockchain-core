82daeededc0d9641e6f9a8bbc2cdcea633f8c87f
Siraaj Khandkar <siraaj@khandkar.net>
2022-06-27 12:50:04 -0400

    Add option to disable iterator_close exception logging

--- src/blockchain_rocks.hrl
+++ src/blockchain_rocks.hrl
@@ -4,7 +4,12 @@
         try
             rocksdb:iterator_close(IteratorHandle)
         catch ErrorClass:ErrorReason ->
-            lager:warning("rocksdb:iterator_close error: ~p:~p", [ErrorClass, ErrorReason])
+            case application:get_env(blockchain, log_rocksdb_iterator_close_errors, true) of
+                true ->
+                    lager:warning("rocksdb:iterator_close error: ~p:~p", [ErrorClass, ErrorReason]);
+                false ->
+                    ok
+            end
         end
     end)()
 ).
