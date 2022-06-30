41403b739a19460293a4af3fd9d8852f2c72f516
Evan Vigil-McClanahan <evan@helium.com>
2022-06-30 16:16:01 -0700

    add some debugging

--- src/transactions/v2/blockchain_txn_poc_receipts_v2.erl
+++ src/transactions/v2/blockchain_txn_poc_receipts_v2.erl
@@ -672,8 +672,8 @@ validate(_POCVersion, Txn, Path, LayerData, LayerHashes, OldLedger) ->
                                                        end,
                                                        {error, invalid_receipt}
                                                end;
-                                           _ ->
-                                               lager:error([{poc_id, POCID}], "receipt not in order"),
+                                           _Unknown ->
+                                               lager:error([{poc_id, POCID}], "receipt not in order, ~p", [_Unknown]),
                                                {error, receipt_not_in_order}
                                        end
                                end,
