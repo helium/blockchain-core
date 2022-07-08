1ea38955f10ce6d5616c6c98978471e9432559cc
Michael Jeffrey <michaeldjeffrey@gmail.com>
2022-07-07 18:07:20 -0700

    add env hooks for test handlers

--- test/sc_client_test_handler.erl
+++ test/sc_client_test_handler.erl
@@ -3,9 +3,11 @@
 -export([handle_response/1, handle_purchase/1]).
 
 handle_response(Resp) ->
+    F = application:get_env(blockchain, sc_client_handle_response_fun, fun(_Resp) -> ok end),
     lager:info("Resp: ~p", [Resp]),
-    ok.
+    F(Resp).
 
 handle_purchase(Purchase) ->
+    F = application:get_env(blockchain, sc_client_handle_purchase_fun, fun(_Resp) -> ok end),
     lager:info("Purchase: ~p", [Purchase]),
-    ok.
+    F(Purchase).
